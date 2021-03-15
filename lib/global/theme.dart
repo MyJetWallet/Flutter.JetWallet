54724b83980a6e5e2e13a3e4b8aa45c3c603fbba
ChiefKender <kendermord@protonmail.com>
2021-03-15 14:22:37 +0200

    signalr_core attempt

--- lib/global/theme.dart
+++ lib/global/theme.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 import 'package:jetwallet/global/const.dart';
 
 final globalSpotTheme = ThemeData(
     primaryColor: primColor,
     scaffoldBackgroundColor: backColor,
+    /* TODO: Solve Google fonts conflict
     fontFamily: GoogleFonts.notoSans().fontFamily,
     textTheme: GoogleFonts.notoSansTextTheme(
       ThemeData(brightness: Brightness.dark).textTheme,
-    ),
+    ),*/
     buttonTheme: ButtonThemeData(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
       buttonColor: primColor,
--- lib/spot_home/signalr/signalr.dart
+++ lib/spot_home/signalr/signalr.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jetwallet/global/const.dart';
-import 'package:signalr_client/signalr_client.dart';
+import 'package:signalr_core/signalr_core.dart';
 
 class SpotSignalR extends StatefulWidget {
   SpotSignalR({Key key}) : super(key: key);
@@ -37,18 +37,18 @@ class _SpotSignalRState extends State<SpotSignalR> {
               padding: const EdgeInsets.all(18.0),
               child: TextButton(
                 child: Text(" Init result: " + initResultString),
-                onPressed: () async {
-                  print("Pressed!");
+                onPressed: () {
+                  /*print("Pressed!");
                   //await hubConnection.start();
-                  if (hubConnection.state == HubConnectionState.Connected) {
+                  /* if (hubConnection.state == HubConnectionState.connected) {
                     await hubConnection.invoke(
                       "Init",
                     );
                     print("Connected!");
-                  }
+                  }*/
                   setState(() {
                     // if (initResult != null) initResultString = initResult.toString();
-                  });
+                  });*/
                 },
               ),
             ),
@@ -60,7 +60,11 @@ class _SpotSignalRState extends State<SpotSignalR> {
 
   void initSignalR() async {
     print(urlSignalR);
-    hubConnection = HubConnectionBuilder().withUrl(urlSignalR).build();
+    hubConnection = HubConnectionBuilder()
+        .withUrl(
+          urlSignalR,
+        )
+        .build();
     print(hubConnection.toString());
     hubConnection.onclose(
         (error) => print('initSignalR: HubConnection: Connection closed'));
