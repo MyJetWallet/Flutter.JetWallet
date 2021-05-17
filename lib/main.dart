3bb09454d34c420fc88f1e6c00b6feaa38bae667
Eli Stark <elistark.tech@gmail.com>
2021-05-17 17:52:23 +0300

    Added Provider Logger

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'router/ui/router.dart';
+import 'shared/provider_logger.dart';
 import 'shared/theme/theme_data.dart';
 
 void main() {
   runApp(
     ProviderScope(
+      observers: [
+        ProviderLogger(),
+      ],
       child: MyApp(),
     ),
   );
