584f31fb5476ccc1bc3d9f5a9d27da2c1c9c3e00
Eli Stark <elistark.tech@gmail.com>
2021-05-19 12:42:12 +0300

    Remove abstraction from services

--- lib/service/services/authorization/service/authorization_service.dart
+++ lib/service/services/authorization/service/authorization_service.dart
@@ -3,29 +3,25 @@ import 'package:dio/dio.dart';
 import '../model/authorization_refresh_request_model.dart';
 import '../model/authorization_request_model.dart';
 import '../model/authorization_response_model.dart';
-import 'authorization_service_abstract.dart';
 import 'services/authorization_service.dart';
 import 'services/logout_service.dart';
 import 'services/refresh_service.dart';
 
-class AuthorizationService implements AuthorizationServiceAbstract {
+class AuthorizationService {
   AuthorizationService(this.dio);
 
   final Dio dio;
 
-  @override
   Future<void> logout() {
     return logoutService(dio);
   }
 
-  @override
   Future<AuthorizationResponseModel> authorize(
     AuthorizationRequestModel model,
   ) {
     return authorizationService(dio, model);
   }
 
-  @override
   Future<AuthorizationResponseModel> refresh(
       AuthorizationRefreshRequestModel model) {
     return refreshService(dio, model);
