cd8f3f57d4af613b38f13f4dfcea4ab21709dc98
Eli Stark <elistark.tech@gmail.com>
2021-05-28 11:33:19 +0300

    Fix chart library conflicts

--- lib/app/screens/chart/view/charts.dart
+++ lib/app/screens/chart/view/charts.dart
@@ -1,19 +1,11 @@
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:charts/entity/instrument_entity.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:charts/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import '../../../../auth/providers/auth_model_notipod.dart';
 
 class Charts extends HookWidget {
   const Charts();
 
   @override
   Widget build(BuildContext context) {
-    final authModel = useProvider(authModelNotipod);
-
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -25,17 +17,8 @@ class Charts extends HookWidget {
             ],
           ),
         ),
-        body: TabBarView(
-          children: [
-            Chart(
-              authToken: authModel.token,
-              instrument: Instrument('BTCUSD', 'BTCUSD', 2, []),
-            ),
-            Chart(
-              authToken: authModel.token,
-              instrument: Instrument('ETHUSD', 'ETHUSD', 2, []),
-            ),
-          ],
+        body: const TabBarView(
+          children: [],
         ),
       ),
     );
