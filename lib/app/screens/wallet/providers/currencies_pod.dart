87c0d7e6ec314045429b02efba25b809bfe32816
Eli Stark <elistark.tech@gmail.com>
2021-06-04 18:36:05 +0300

    Add tests for balance calculators (#36)
    * Add tests for balance calculators
    
    * Updated special cases for balance calculation

--- lib/app/screens/wallet/providers/currencies_pod.dart
+++ lib/app/screens/wallet/providers/currencies_pod.dart
@@ -1,15 +1,19 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../helpers/accuracy_from.dart';
+import '../helpers/calculate_base_balance.dart';
 import '../models/currency_model.dart';
 import 'assets_spod.dart';
 import 'balances_spod.dart';
 import 'converter_map_fpod.dart';
+import 'instruments_spod.dart';
 import 'prices_spod.dart';
 
 final currenciesPod = Provider<List<CurrencyModel>>((ref) {
   final assets = ref.watch(assetsSpod);
   final balances = ref.watch(balancesSpod);
   final prices = ref.watch(pricesSpod);
+  final instruments = ref.watch(instrumentsSpod);
   final converter = ref.watch(converterMapFpod);
 
   final currencies = <CurrencyModel>[];
@@ -55,23 +59,30 @@ final currenciesPod = Provider<List<CurrencyModel>>((ref) {
     }
   });
 
-  prices.whenData((pricesData) {
-    converter.whenData((converterData) {
-      if (currencies.isNotEmpty) {
-        for (final currency in currencies) {
-          final index = currencies.indexOf(currency);
+  instruments.whenData((instrumentsData) {
+    final instruments = instrumentsData.instruments;
 
-          final baseBalance = currencies[index].calculateBaseBalance(
-            prices: pricesData,
-            currencies: currencies,
-            converter: converterData,
-          );
+    prices.whenData((pricesData) {
+      converter.whenData((converterData) {
+        if (currencies.isNotEmpty) {
+          for (final currency in currencies) {
+            final index = currencies.indexOf(currency);
 
-          currencies[index] = currency.copyWith(
-            baseBalance: baseBalance,
-          );
+            final baseBalance = calculateBaseBalance(
+              accuracy: accuracyFrom('USD', instruments),
+              baseSymbol: 'USD',
+              assetSymbol: currency.symbol,
+              assetBalance: currency.assetBalance,
+              prices: pricesData.prices,
+              converter: converterData,
+            );
+
+            currencies[index] = currency.copyWith(
+              baseBalance: baseBalance,
+            );
+          }
         }
-      }
+      });
     });
   });
 
---
+++ test/app/screens/wallet/helpers/accuracy_from_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jetwallet/app/screens/wallet/helpers/accuracy_from.dart';
+import 'package:jetwallet/service/services/signal_r/model/instruments_model.dart';
+
+void main() {
+  test('Accuracy from USD', () async {
+    final result = accuracyFrom('USD', instruments);
+
+    expect(result, 2);
+  });
+  test('Accuracy from BTC', () async {
+    final result = accuracyFrom('BTC', instruments);
+
+    expect(result, 8);
+  });
+}
+
+const instruments = <InstrumentModel>[
+  InstrumentModel(
+    symbol: '',
+    baseAsset: '',
+    quoteAsset: 'USD',
+    accuracy: 2,
+    minVolume: 0,
+    maxVolume: 0,
+    maxOppositeVolume: 0,
+  ),
+  InstrumentModel(
+    symbol: '',
+    baseAsset: '',
+    quoteAsset: 'BTC',
+    accuracy: 8,
+    minVolume: 0,
+    maxVolume: 0,
+    maxOppositeVolume: 0,
+  ),
+];
