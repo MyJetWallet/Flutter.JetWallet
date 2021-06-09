e43d08514f887cebec425404ec8e90faf0f79ae1
Eli Stark <elistark.tech@gmail.com>
2021-06-09 13:22:00 +0300

    blockchain service restructure

--- lib/service/services/blockchain/service/services/withdrawal_service.dart
+++ lib/service/services/blockchain/service/services/withdrawal_service.dart
@@ -2,8 +2,6 @@ import 'package:dio/dio.dart';
 
 import '../../../../shared/constants.dart';
 import '../../../../shared/helpers/handle_api_responses.dart';
-import '../../dto/withdrawal/withdrawal_request_dto.dart';
-import '../../dto/withdrawal/withdrawal_response_dto.dart';
 import '../../model/withdrawal/withdrawal_request_model.dart';
 import '../../model/withdrawal/withdrawal_response_model.dart';
 
@@ -11,18 +9,14 @@ Future<WithdrawalResponseModel> withdrawalService(
   Dio dio,
   WithdrawalRequestModel model,
 ) async {
-  final requestDto = WithdrawalRequestDto.fromModel(model);
-
   final response = await dio.post(
     '$walletApiBaseUrl/blockchain/withdrawal',
-    data: requestDto.toJson(),
+    data: model.toJson(),
   );
 
   final responseData = response.data as Map<String, dynamic>;
 
   final data = handleFullResponse<Map>(responseData);
 
-  final responseDto = WithdrawalResponseDto.fromJson(data);
-
-  return responseDto.toModel();
+  return WithdrawalResponseModel.fromJson(data);
 }
