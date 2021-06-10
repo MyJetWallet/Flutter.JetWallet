485509edb7df2c096fd0aae30998e6603b352491
Eli Stark <elistark.tech@gmail.com>
2021-06-10 10:23:50 +0300

    ci

--- lib/shared/services/local_storage_service.dart
+++ lib/shared/services/local_storage_service.dart
@@ -1,21 +1,21 @@
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 // KEYS
 const refreshTokenKey = 'refreshTokenKey';
 
 class LocalStorageService {
   Future<String?> getString(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(key);
+    // final prefs = await SharedPreferences.getInstance();
+    return 'prefs.getString(key)';
   }
 
   Future<void> setString(String key, String value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(key, value);
+    // final prefs = await SharedPreferences.getInstance();
+    await 'prefs.setString(key, value)';
   }
 
   Future<void> clearStorage() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.clear();
+    // final prefs = await SharedPreferences.getInstance();
+    await 'prefs.clear()';
   }
 }
