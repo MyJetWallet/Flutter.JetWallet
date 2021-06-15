5726e4a38013c348f6ad2efd21f6cd7f5117de8c
Eli Stark <elistark.tech@gmail.com>
2021-06-15 09:31:16 +0300

    Add logging to SwapService

--- lib/service/services/swap/service/swap_service.dart
+++ lib/service/services/swap/service/swap_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:logging/logging.dart';
 
 import '../model/execute_quote/execute_quote_request_model.dart';
 import '../model/execute_quote/execute_quote_response_model.dart';
@@ -11,6 +12,8 @@ class SwapService {
   SwapService(this.dio);
 
   final Dio dio;
+
+  static final logger = Logger('SwapService');
 
   Future<ExecuteQuoteResponseModel> executeQuote(
     ExecuteQuoteRequestModel model,
