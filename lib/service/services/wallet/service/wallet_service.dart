ef7c57a6ca07dae252bfacac3a4210b3d847c7fa
Eli Stark <elistark.tech@gmail.com>
2021-06-15 09:33:42 +0300

    Add logging to WalletService

--- lib/service/services/wallet/service/wallet_service.dart
+++ lib/service/services/wallet/service/wallet_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:logging/logging.dart';
 
 import '../model/asset_converter_map_model.dart';
 import 'services/asset_converter_map_service.dart';
@@ -7,6 +8,8 @@ class WalletService {
   WalletService(this.dio);
 
   final Dio dio;
+
+  static final logger = Logger('WalletService');
 
   Future<AssetConverterMapModel> assetConverterMap(String symbol) {
     return assetConverterMapService(dio, symbol);
