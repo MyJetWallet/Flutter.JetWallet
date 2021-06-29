87df65cb1dfc7c437a01252795549ab253c2f0ba
Eli Stark <elistark.tech@gmail.com>
2021-06-29 16:20:46 +0300

    Update dynamic link navigation flow

--- lib/shared/providers/background/dynamic_link_pod.dart
+++ lib/shared/providers/background/dynamic_link_pod.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import '../../../auth/screens/reset_password/view/reset_password.dart';
 
+import '../../../auth/screens/email_verification/view/email_verification.dart';
+import '../../../auth/screens/reset_password/view/reset_password.dart';
 import '../../../service_providers.dart';
 import '../other/navigator_key_pod.dart';
 
-const command = 'jw_command';
-const emailVerification = 'emailverification';
-const forgotPassword = 'ForgotPassword';
-const token = 'jw_token';
+const _token = 'jw_token';
+const _command = 'jw_command';
+const _emailVerification = 'emailverification';
+const _forgotPassword = 'ForgotPassword';
 
 final dynamicLinkPod = Provider<void>(
   (ref) {
@@ -19,24 +20,23 @@ final dynamicLinkPod = Provider<void>(
       handler: (link) {
         final parameters = link.queryParameters;
 
-        navigatorKey.currentState!.push(
+        navigatorKey.currentState!.pushAndRemoveUntil(
           MaterialPageRoute(
             builder: (context) {
-              if (parameters[command] == emailVerification) {
-                return EmailVerification(
-                  deepLinkParameters: parameters,
-                );
-              } else if (parameters[command] == forgotPassword) {
+              if (parameters[_command] == _emailVerification) {
+                return const EmailVerification();
+              } else if (parameters[_command] == _forgotPassword) {
                 return ResetPassword(
-                  token: parameters[token],
+                  token: parameters[_token],
                 );
               } else {
-                return UndefinedDeepLink(
+                return _UndefinedDeepLink(
                   deepLinkParameters: parameters,
                 );
               }
             },
           ),
+          (route) => route.isFirst == true,
         );
       },
     );
@@ -44,38 +44,8 @@ final dynamicLinkPod = Provider<void>(
   name: 'dynamicLinkPod',
 );
 
-// TODO: Refactor, we need to review our current arch of the app
-class EmailVerification extends StatelessWidget {
-  const EmailVerification({
-    Key? key,
-    required this.deepLinkParameters,
-  }) : super(key: key);
-
-  final Map<String, String> deepLinkParameters;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Email Verification',
-        ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            for (final parameter in deepLinkParameters.entries)
-              Text('${parameter.key}: ${parameter.value}')
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// TODO: Refactor, we need to review our current arch of the app
-class UndefinedDeepLink extends StatelessWidget {
-  const UndefinedDeepLink({
+class _UndefinedDeepLink extends StatelessWidget {
+  const _UndefinedDeepLink({
     Key? key,
     required this.deepLinkParameters,
   }) : super(key: key);
