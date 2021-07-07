087a2a05224506cc2b1b486c4517fca95c81c6b9
Eli Stark <elistark.tech@gmail.com>
2021-07-07 12:38:38 +0300

    Reflect on the review

--- lib/development/logs_screen/view/logs_screen.dart
+++ lib/development/logs_screen/view/logs_screen.dart
@@ -1,12 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../shared/providers/background/log_records_notipod.dart';
-import '../helper/encode_query_parameters.dart';
+import '../helper/beatify_logs_for_support.dart';
 import '../helper/make_log_pretty.dart';
+import '../helper/send_logs_to_support.dart';
 
 class LogsScreen extends HookWidget {
   const LogsScreen({
@@ -15,36 +14,21 @@ class LogsScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final notipod = useProvider(logRecordsNotipod);
+    final logs = useProvider(logRecordsNotipod).toList().reversed;
 
     return Scaffold(
       appBar: AppBar(),
       body: ListView(
         children: [
-          if (notipod.isNotEmpty)
-            for (final log in notipod.toList().reversed)
-              Text(makeLogPretty(log, '\n'))
+          if (logs.isNotEmpty)
+            for (final log in logs) Text(makeLogPretty(log, '\n'))
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          final buffer = StringBuffer();
-
-          for (final log in notipod.toList().reversed) {
-            buffer.write(makeLogPretty(log, '<br>'));
-            buffer.write('<br>');
-          }
-
-          final _emailLaunchUri = Uri(
-            scheme: 'mailto',
-            path: 'illia.s@smplt.net, vladimir.m@smplt.net',
-            query: encodeQueryParameters({
-              'subject': 'JetWallet Logs',
-              'body': buffer.toString(),
-            }),
+          sendLogsToSupport(
+            beatifyLogsForSupport(logs),
           );
-
-          launch(_emailLaunchUri.toString());
         },
         child: const Icon(
           Icons.send,
