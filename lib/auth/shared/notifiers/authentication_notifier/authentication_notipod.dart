8d024e594889d97e291d6e28f3c1a656c392a98e
Eli Stark <elistark.tech@gmail.com>
2021-07-16 10:48:49 +0300

    Redesign Register and Login flow

--- lib/auth/shared/notifiers/authentication_notifier/authentication_notipod.dart
+++ lib/auth/shared/notifiers/authentication_notifier/authentication_notipod.dart
@@ -3,8 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../../../router/provider/router_stpod/router_stpod.dart';
 import '../../../../../shared/providers/other/navigator_key_pod.dart';
 import '../../../../../shared/providers/service_providers.dart';
-import '../auth_model_notifier/auth_model_notipod.dart';
-import '../credentials_notifier/credentials_notipod.dart';
+import '../auth_info_notifier/auth_info_notipod.dart';
 import 'authentication_notifier.dart';
 import 'authentication_union.dart';
 
@@ -12,9 +11,7 @@ final authenticationNotipod =
     StateNotifierProvider<AuthenticationNotifier, AuthenticationUnion>(
   (ref) {
     final router = ref.watch(routerStpod.notifier);
-    final credentialsState = ref.watch(credentialsNotipod);
-    final credentialsNotifier = ref.watch(credentialsNotipod.notifier);
-    final authModelNotifier = ref.watch(authModelNotipod.notifier);
+    final authInfoN = ref.watch(authInfoNotipod.notifier);
     final authService = ref.watch(authServicePod);
     final storageService = ref.watch(localStorageServicePod);
     final navigatorKey = ref.watch(navigatorKeyPod);
@@ -22,9 +19,7 @@ final authenticationNotipod =
 
     return AuthenticationNotifier(
       router: router,
-      credentialsState: credentialsState,
-      credentialsNotifier: credentialsNotifier,
-      authModelNotifier: authModelNotifier,
+      authInfoN: authInfoN,
       authService: authService,
       storageService: storageService,
       navigatorKey: navigatorKey,
