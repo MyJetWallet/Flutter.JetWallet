0185d1932936d0be1858b2397075592f64cd0083
Vladimir <vovkamedvedev5@gmail.com>
2021-07-27 12:51:12 +0300

    Add sort by dayPercentChange for gainers/loosers (#105)
    Co-authored-by: Eli Stark <elistark.tech@gmail.com>

--- lib/app/screens/market/provider/market_gainers_pod.dart
+++ lib/app/screens/market/provider/market_gainers_pod.dart
@@ -5,6 +5,9 @@ import 'market_items_pod.dart';
 
 final marketGainersPod = Provider.autoDispose<List<MarketItemModel>>((ref) {
   final items = ref.watch(marketItemsPod);
+  final gainers = items.where((item) => item.dayPercentChange > 0).toList();
 
-  return items.where((item) => item.dayPercentChange > 0).toList();
+  gainers.sort((a, b) => b.dayPercentChange.compareTo(a.dayPercentChange));
+
+  return gainers;
 });
--- lib/app/screens/market/provider/market_loosers_pod.dart
+++ lib/app/screens/market/provider/market_loosers_pod.dart
@@ -5,6 +5,9 @@ import 'market_items_pod.dart';
 
 final marketLoosersPod = Provider.autoDispose<List<MarketItemModel>>((ref) {
   final items = ref.watch(marketItemsPod);
+  final loosers = items.where((item) => item.dayPercentChange < 0).toList();
 
-  return items.where((item) => item.dayPercentChange < 0).toList();
+  loosers.sort((a, b) => a.dayPercentChange.compareTo(b.dayPercentChange));
+
+  return loosers;
 });
