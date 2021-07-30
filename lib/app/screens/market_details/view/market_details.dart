50d75179550406c1e667eb5acf51934389969231
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2021-07-30 18:20:47 +0300

    Add l10n import

--- lib/app/screens/market_details/view/market_details.dart
+++ lib/app/screens/market_details/view/market_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../service/services/wallet/model/market_info/market_info_request_model.dart';
