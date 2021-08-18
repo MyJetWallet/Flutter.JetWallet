8998fa1de95a742198432df75aea81afb0fc2ff7
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2021-08-18 13:27:18 +0300

    Convert to relative import

--- lib/app/screens/market_details/view/components/news_block/components/news_item.dart
+++ lib/app/screens/market_details/view/components/news_block/components/news_item.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:jetwallet/app/screens/market_details/view/components/news_block/components/news_item_text.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 import '../../../../../../../service/services/wallet/model/news/news_response_model.dart';
 import '../../../../../../../shared/components/spacers.dart';
+import 'news_item_text.dart';
 
 class NewsItem extends StatelessWidget {
   const NewsItem({
