2047211d74c293f4f767fc22c2bc37f5f9c3baa2
Eli Stark <elistark.tech@gmail.com>
2021-08-19 17:51:49 +0300

    Add new dropdown sheet to convert

--- lib/app/shared/features/convert/view/components/convert_row/convert_row.dart
+++ lib/app/shared/features/convert/view/components/convert_row/convert_row.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../../../../../shared/components/spacers.dart';
+import '../../../../../components/asset_tile/asset_tile.dart';
+import '../../../../../components/basic_bottom_sheet/basic_bottom_sheet.dart';
+import '../../../../../components/text/asset_sheet_header.dart';
 import '../../../../../models/currency_model.dart';
 import 'components/convert_asset_input.dart';
-import 'components/convert_dropdown.dart';
+import 'components/convert_dropdown_button.dart';
 
 class ConvertRow extends StatelessWidget {
   const ConvertRow({
@@ -28,6 +30,37 @@ class ConvertRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showDropdownSheet() {
+      showBasicBottomSheet(
+        context: context,
+        scrollable: true,
+        color: const Color(0xFF4F4F4F),
+        pinned: AssetSheetHeader(
+          text: fromAsset ? 'From' : 'To',
+        ),
+        children: [
+          for (final item in currencies)
+            AssetTile(
+              currency: item,
+              onTap: () {
+                if (currency == item) {
+                  Navigator.pop(context);
+                } else {
+                  Navigator.pop(context, item);
+                }
+              },
+              selectedBorder: currency == item,
+            ),
+        ],
+        then: (value) {
+          if (value is CurrencyModel) {
+            onDropdown(value);
+          }
+        },
+        onDissmis: () => Navigator.pop(context),
+      );
+    }
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.end,
@@ -37,20 +70,9 @@ class ConvertRow extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  Image.network(
-                    currency.iconUrl,
-                    width: 35.w,
-                    height: 35.w,
-                  ),
-                  const SpaceW10(),
-                  ConvertDropdown(
-                    value: currency,
-                    currencies: currencies,
-                    onChanged: onDropdown,
-                  ),
-                ],
+              ConvertDropdownButton(
+                onTap: () => _showDropdownSheet(),
+                currency: currency,
               ),
               if (fromAsset)
                 FittedBox(
