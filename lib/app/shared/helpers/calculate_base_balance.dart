903e0720d2f0640a493ff0694dc63fe936c8e5a2
Eli Stark <elistark.tech@gmail.com>
2021-08-23 11:49:09 +0300

    Remove functionality of bid-ask feature

--- lib/app/shared/helpers/calculate_base_balance.dart
+++ lib/app/shared/helpers/calculate_base_balance.dart
@@ -1,8 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../service/services/signal_r/model/base_prices_model.dart';
-import '../../../service/services/signal_r/model/prices_model.dart';
-import '../../../service/services/wallet/model/asset_converter_map/asset_converter_map_model.dart';
 import '../providers/base_currency_pod/base_currency_pod.dart';
 import '../providers/signal_r/base_prices_spod.dart';
 
@@ -46,66 +44,4 @@ double calculateBaseBalance({
   final result = assetBalance * assetPrice.currentPrice;
 
   return double.parse(result.toStringAsFixed(accuracy));
-}
-
-/// returns [-1] if there is no prices available for specific asset
-double calculateBaseBalanceWithMap({
-  required int accuracy,
-  required String assetSymbol,
-  required double assetBalance,
-  required List<PriceModel> prices,
-  required AssetConverterMapModel converter,
-}) {
-  var result = assetBalance;
-  var isAvailable = false;
-
-  if (assetBalance == 0) {
-    return 0;
-  }
-
-  if (converter.baseAssetSymbol == assetSymbol) {
-    return assetBalance;
-  }
-
-  for (final map in converter.maps) {
-    if (map.assetSymbol == assetSymbol) {
-      map.operations.sort((a, b) => a.order.compareTo(b.order));
-
-      for (final operation in map.operations) {
-        final pair = operation.instrumentPair;
-        final isMultiply = operation.isMultiply;
-
-        for (final price in prices) {
-          if (pair == price.id) {
-            if (operation.useBid) {
-              result = _calculate(result, isMultiply, price.bid);
-            } else {
-              result = _calculate(result, isMultiply, price.ask);
-            }
-            isAvailable = true;
-            break;
-          }
-        }
-      }
-      break;
-    }
-  }
-
-  if (isAvailable) {
-    return double.parse(result.toStringAsFixed(accuracy));
-  } else {
-    return -1;
-  }
-}
-
-double _calculate(double result, bool isMultiply, double price) {
-  double value;
-
-  if (isMultiply) {
-    value = result * price;
-  } else {
-    value = result / price;
-  }
-
-  return value;
 }
