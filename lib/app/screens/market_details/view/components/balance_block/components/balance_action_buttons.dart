23180c495e3ce0484d330cb94149a1ff8ea0cd8b
Vladimir <vovkamedvedev5@gmail.com>
2021-09-01 10:54:31 +0300

    SPUI-332 > Add navigation to buttons market details (#134)
    * Add navigation to action buttons on Market Details
    
    * Fix linter
    
    * Fix pr comment
    
    * Fix pr comment
    
    * small changes
    
    Co-authored-by: Eli Stark <elistark.tech@gmail.com>

--- lib/app/screens/market_details/view/components/balance_block/components/balance_action_buttons.dart
+++ lib/app/screens/market_details/view/components/balance_block/components/balance_action_buttons.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../../../../shared/components/buttons/app_buton_white.dart';
 import '../../../../../../../shared/components/spacers.dart';
+import '../../../../../../../shared/helpers/navigator_push.dart';
+import '../../../../../../shared/features/currency_buy/view/curency_buy.dart';
+import '../../../../../../shared/features/currency_sell/view/currency_sell.dart';
+import '../../../../../../shared/providers/currencies_pod/currencies_pod.dart';
 import '../../../../../market/model/market_item_model.dart';
+import '../../../../helper/currency_from.dart';
 
-class BalanceActionButtons extends StatelessWidget {
+class BalanceActionButtons extends HookWidget {
   const BalanceActionButtons({
     Key? key,
     required this.marketItem,
@@ -14,12 +21,24 @@ class BalanceActionButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currency = currencyFrom(
+      useProvider(currenciesPod),
+      marketItem.associateAsset,
+    );
+
     return Row(
       children: [
         Expanded(
           child: AppButtonWhite(
             name: 'Buy',
-            onTap: () {},
+            onTap: () {
+              navigatorPush(
+                context,
+                CurrencyBuy(
+                  currency: currency,
+                ),
+              );
+            },
           ),
         ),
         if (!marketItem.isBalanceEmpty) ...[
@@ -27,7 +46,14 @@ class BalanceActionButtons extends StatelessWidget {
           Expanded(
             child: AppButtonWhite(
               name: 'Sell',
-              onTap: () {},
+              onTap: () {
+                navigatorPush(
+                  context,
+                  CurrencySell(
+                    currency: currency,
+                  ),
+                );
+              },
             ),
           ),
         ]
