cb9174c63c1c23fb0c0c2278f053b21e36fc5357
Eli Stark <elistark.tech@gmail.com>
2021-09-14 13:51:44 +0300

    SPUI-379 > Assets are not disposed after logout

--- lib/router/provider/signalr_init_fpod.dart
+++ lib/router/provider/signalr_init_fpod.dart
@@ -4,10 +4,13 @@ import 'package:logging/logging.dart';
 import '../../shared/providers/service_providers.dart';
 import 'authorized_stpod/authorized_stpod.dart';
 import 'authorized_stpod/authorized_union.dart';
+import 'router_stpod/router_stpod.dart';
 
 final signalRInitFpod = FutureProvider<void>((ref) async {
   final _logger = Logger('signalRInitFpod');
 
+  /// Watching for authentication changes
+  ref.watch(routerStpod);
   final authorized = ref.watch(authorizedStpod);
   final signalRService = ref.watch(signalRServicePod);
 
