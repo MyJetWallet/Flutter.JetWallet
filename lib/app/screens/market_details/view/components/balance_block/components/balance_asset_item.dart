92dbcf01ee924b727cf7a93798f42d0852692acb
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2021-09-15 11:55:32 +0300

    Add market details real-time update

--- lib/app/screens/market_details/view/components/balance_block/components/balance_asset_item.dart
+++ lib/app/screens/market_details/view/components/balance_block/components/balance_asset_item.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../../../../shared/components/spacers.dart';
 import '../../../../../../../shared/helpers/navigator_push.dart';
 import '../../../../../../shared/components/asset_icon.dart';
 import '../../../../../market/model/market_item_model.dart';
+import '../../../../../market/provider/market_items_pod.dart';
+import '../../../../../wallet/helper/market_item_from.dart';
 import '../../../../../wallet/view/empty_wallet.dart';
 import '../../../../../wallet/view/wallet.dart';
 
-class BalanceAssetItem extends StatelessWidget {
+class BalanceAssetItem extends HookWidget {
   const BalanceAssetItem({
     Key? key,
-    required this.marketItem,
+    required this.assetId,
   }) : super(key: key);
 
-  final MarketItemModel marketItem;
+  final String assetId;
 
   @override
   Widget build(BuildContext context) {
+    final marketItem = marketItemFrom(
+      useProvider(marketItemsPod),
+      assetId,
+    );
+
     return InkWell(
-      onTap: () => _navigateToWallet(context),
+      onTap: () => _navigateToWallet(context, marketItem),
       child: Container(
         height: 64.h,
         padding: EdgeInsets.symmetric(
@@ -74,19 +83,19 @@ class BalanceAssetItem extends StatelessWidget {
     );
   }
 
-  void _navigateToWallet(BuildContext context) {
-    if (marketItem.isBalanceEmpty) {
+  void _navigateToWallet(BuildContext context, MarketItemModel item) {
+    if (item.isBalanceEmpty) {
       navigatorPush(
         context,
         EmptyWallet(
-          assetName: marketItem.name,
+          assetName: item.name,
         ),
       );
     } else {
       navigatorPush(
         context,
         Wallet(
-          marketItem: marketItem,
+          marketItem: item,
         ),
       );
     }
