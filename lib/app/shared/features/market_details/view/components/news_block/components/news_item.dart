2ac1f6c0e43af9a17df0dad6ac1e52f6848e8df9
Vladimir <vovkamedvedev5@gmail.com>
2021-09-23 11:33:11 +0300

    SPUI-326 > Fix news pagination (#166)
    * Fix news pagination
    
    * Refactor date formatting to helper
    
    * Fix linter

--- lib/app/shared/features/market_details/view/components/news_block/components/news_item.dart
+++ lib/app/shared/features/market_details/view/components/news_block/components/news_item.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:timeago/timeago.dart' as timeago;
 
 import '../../../../../../../../service/services/news/model/news_response_model.dart';
 import '../../../../../../../../shared/components/spacers.dart';
 import '../../../../../../../../shared/helpers/launch_url.dart';
+import '../../../../helper/format_news_date.dart';
 import 'news_item_text.dart';
 
 class NewsItem extends StatelessWidget {
@@ -34,9 +34,7 @@ class NewsItem extends StatelessWidget {
                 text: '•',
               ),
               NewsItemText(
-                text: ' ${timeago.format(
-                  DateTime.parse(item.timestamp),
-                )}',
+                text: formatNewsDate(item.timestamp),
               ),
             ],
           ),
@@ -46,7 +44,9 @@ class NewsItem extends StatelessWidget {
             style: TextStyle(
               fontSize: 16.sp,
             ),
-          )
+          ),
+          const SpaceH4(),
+          const Divider(),
         ],
       ),
     );
