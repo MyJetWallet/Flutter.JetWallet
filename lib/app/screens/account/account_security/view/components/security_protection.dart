47bd3b22f3897b09d18d6ec117af00b8b8899039
Eli Stark <elistark.tech@gmail.com>
2021-10-04 12:47:26 +0300

    SPUI-407 > Add security protection levels

--- lib/app/screens/account/account_security/view/components/security_protection.dart
+++ lib/app/screens/account/account_security/view/components/security_protection.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../../../shared/components/spacers.dart';
+import '../../../../../../shared/notifiers/user_info_notifier/user_info_notipod.dart';
+import '../../helper/protection_level.dart';
 
-class SecurityProtection extends StatelessWidget {
+class SecurityProtection extends HookWidget {
   const SecurityProtection({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final userInfo = useProvider(userInfoNotipod);
+    final level = protectionLevel(userInfo);
+
     return Container(
       width: double.infinity,
       padding: EdgeInsets.all(20.r),
@@ -22,11 +29,11 @@ class SecurityProtection extends StatelessWidget {
           Icon(
             Icons.shield,
             size: 46.r,
-            color: Colors.green,
+            color: level.color,
           ),
           const SpaceH10(),
           Text(
-            'Hardcoded protection level',
+            '${level.name} protection level',
             style: TextStyle(
               fontSize: 16.sp,
               fontWeight: FontWeight.bold,
