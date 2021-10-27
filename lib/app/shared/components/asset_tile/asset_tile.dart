abaa12472a6352fa61a8982c19875ce378eb35d0
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2021-10-27 23:52:51 +0300

    Fix images rendering

--- lib/app/shared/components/asset_tile/asset_tile.dart
+++ lib/app/shared/components/asset_tile/asset_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../../../../shared/components/spacers.dart';
@@ -79,10 +80,16 @@ class AssetTile extends HookWidget {
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.network(
+              SvgPicture.network(
                 currency.iconUrl,
                 width: 24.w,
                 height: 24.w,
+                placeholderBuilder: (_) {
+                  return Icon(
+                    Icons.error,
+                    size: 30.r,
+                  );
+                },
               ),
               const SpaceW10(),
               AssetTileColumn(
