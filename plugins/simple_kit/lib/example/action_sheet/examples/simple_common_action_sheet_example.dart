3949bcd8ebd313acfa7fe09ac8c1aae9ecb91bdd
Eli Stark <elistark.tech@gmail.com>
2021-11-04 16:18:14 +0200

    [simple_kit]: Add common action sheet

--- plugins/simple_kit/lib/example/action_sheet/examples/simple_common_action_sheet_example.dart
+++ plugins/simple_kit/lib/example/action_sheet/examples/simple_common_action_sheet_example.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../simple_kit.dart';
+import '../../../src/action_sheet/components/basic_bottom_sheet/basic_bottom_sheet.dart';
+import '../../../src/action_sheet/components/simple_bottom_sheet_header.dart';
+
 class SimpleCommonActionSheetExample extends StatelessWidget {
   const SimpleCommonActionSheetExample({Key? key}) : super(key: key);
 
@@ -11,6 +15,35 @@ class SimpleCommonActionSheetExample extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                showBasicBottomSheet(
+                  context: context,
+                  pinned: const SBottomSheetHeader(
+                    name: 'Deposit With',
+                  ),
+                  children: [
+                    SFiatSheetItem(
+                      icon: const SActionBuyIcon(),
+                      name: 'Fiat Currency',
+                      amount: '\$0.00',
+                      onTap: () {},
+                    ),
+                    SAssetSheetItem(
+                      icon: const SActionBuyIcon(),
+                      name: 'Asset Name',
+                      amount: '\$0.00',
+                      description: 'Asset balance',
+                      onTap: () {},
+                    ),
+                    const SpaceH20(),
+                  ],
+                );
+              },
+              child: const Text('Show Common Action Sheet'),
+            )
+          ],
         ),
       ),
     );
