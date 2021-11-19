9d2235b9b4f3a0ea08b36900049c1374541a363f
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2021-11-19 13:22:41 +0200

    Fix import

--- lib/router/view/components/app_init.dart
+++ lib/router/view/components/app_init.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:jetwallet/auth/screens/splash/splash_screen.dart';
 
 import '../../../app/screens/navigation/view/navigation.dart';
 import '../../../auth/screens/email_verification/view/email_verification.dart';
 import '../../../auth/screens/onboarding/onboarding_screen.dart';
+import '../../../auth/screens/splash/splash_screen.dart';
 import '../../../shared/features/pin_screen/model/pin_flow_union.dart';
 import '../../../shared/features/pin_screen/view/pin_screen.dart';
 import '../../../shared/features/two_fa/two_fa_phone/model/two_fa_phone_trigger_union.dart';
