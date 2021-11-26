c1d40e7639a2b7c6fc8c9bc9bc8ac4f708f70059
Eli Stark <elistark.tech@gmail.com>
2021-11-26 13:30:11 +0200

    SPUI-714 >Add profile info to UserInfo model (#267)

--- lib/shared/providers/service_providers.dart
+++ lib/shared/providers/service_providers.dart
@@ -15,6 +15,7 @@ import '../../service/services/news/news_service.dart';
 import '../../service/services/notification/service/notification_service.dart';
 import '../../service/services/operation_history/operation_history_service.dart';
 import '../../service/services/phone_verification/service/phone_verification_service.dart';
+import '../../service/services/profile/service/profile_service.dart';
 import '../../service/services/signal_r/service/signal_r_service.dart';
 import '../../service/services/swap/service/swap_service.dart';
 import '../../service/services/transfer/service/transfer_service.dart';
@@ -152,4 +153,10 @@ final transferServicePod = Provider<TransferService>((ref) {
 
 final contactsServicePod = Provider<ContactService>((ref) {
   return ContactService();
+});
+
+final profileServicePod = Provider<ProfileService>((ref) {
+  final dio = ref.watch(dioPod);
+
+  return ProfileService(dio);
 });
