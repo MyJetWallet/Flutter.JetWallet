7ef1a5e7c440134085ddde3433957c863817e68c
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2021-12-07 16:20:45 +0200

    Add android notification icon

--- lib/shared/services/push_notification_service.dart
+++ lib/shared/services/push_notification_service.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:logging/logging.dart';
 
@@ -24,8 +23,7 @@ class PushNotificationService {
       _channel.id,
       _channel.name,
       channelDescription: _channel.description,
-      color: Colors.blue,
-      icon: '@mipmap/ic_launcher',
+      icon: '@drawable/ic_notification',
     ),
   );
 
