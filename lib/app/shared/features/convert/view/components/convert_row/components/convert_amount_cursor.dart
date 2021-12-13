33821900a3462737adfc8447124fd8dc91f02f00
Eli Stark <elistark.tech@gmail.com>
2021-12-13 13:17:19 +0200

    SPUI-761 > Fix convert cursor bug (#303)

--- lib/app/shared/features/convert/view/components/convert_row/components/convert_amount_cursor.dart
+++ lib/app/shared/features/convert/view/components/convert_row/components/convert_amount_cursor.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:simple_kit/simple_kit.dart';
 
-class ConvertAmountCursor extends StatelessWidget {
+class ConvertAmountCursor extends HookWidget {
   const ConvertAmountCursor({Key? key}) : super(key: key);
 
   @override
