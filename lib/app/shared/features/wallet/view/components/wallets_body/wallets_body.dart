f60ce8056e8276f00bfce09810c660fc35a522e1
Vladimir <vovkamedvedev5@gmail.com>
2021-12-14 17:49:15 +0200

    SPUI-533/516/517 > Wallet screen UI (#306)
    * Add Wallet Screen UI
    
    * Fix linter
    
    * Fix pr comments

--- lib/app/shared/features/wallet/view/components/wallets_body/wallets_body.dart
+++ lib/app/shared/features/wallet/view/components/wallets_body/wallets_body.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../../../../shared/components/loaders/loader.dart';
-import '../../../../../../screens/market/model/market_item_model.dart';
 import '../../../../../../screens/market/provider/market_items_pod.dart';
 import '../../../helper/assets_with_balance_from.dart';
 import '../../../notifier/operation_history_notipod.dart';
@@ -22,6 +20,7 @@ class Wallets extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     final transactionsHistoryInit = useProvider(
       operationHistoryInitFpod(assetId),
     );
@@ -35,30 +34,23 @@ class Wallets extends HookWidget {
 
     return transactionsHistoryInit.when(
       data: (_) {
-        return RefreshIndicator(
-          onRefresh: () => _refresh(
-            context,
-            _currentAssetFrom(itemsWithBalance),
-          ),
-          child: SingleChildScrollView(
-            child: SizedBox(
-              height: 1.0.sh,
-              width: double.infinity,
-              child: PageView(
-                controller: _pageController,
-                onPageChanged: (index) {
-                  transactionHistoryN.initOperationHistory(
-                    itemsWithBalance[index].associateAsset,
-                  );
-                },
-                children: [
-                  for (final item in itemsWithBalance)
-                    WalletBody(
-                      assetId: item.associateAsset,
-                      currentPage: _currentPage(),
-                    ),
-                ],
-              ),
+        return SingleChildScrollView(
+          child: SizedBox(
+            height: screenHeight,
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                transactionHistoryN.initOperationHistory(
+                  itemsWithBalance[index].associateAsset,
+                );
+              },
+              children: [
+                for (final item in itemsWithBalance)
+                  WalletBody(
+                    assetId: item.associateAsset,
+                    currentPage: _currentPage(),
+                  ),
+              ],
             ),
           ),
         );
@@ -67,16 +59,6 @@ class Wallets extends HookWidget {
       error: (e, _) => Text('$e'),
     );
   }
-
-  Future<void> _refresh(BuildContext context, String assetId) {
-    final transactionHistoryN = context.read(
-      operationHistoryNotipod.notifier,
-    );
-    return transactionHistoryN.initOperationHistory(assetId);
-  }
-
-  String _currentAssetFrom(List<MarketItemModel> itemsWithBalance) =>
-      itemsWithBalance[(_pageController.page ?? 0).toInt()].associateAsset;
 
   int _currentPage() =>
       (_pageController.hasClients ? (_pageController.page ?? 0) : 0).toInt();
