7a5fe8da48ee7c995ab0b72ca2ae09ca7db7dff7
Denis Martych <93989208+DenisMartych@users.noreply.github.com>
2021-12-16 19:52:01 +0200

    SPUI-794 > Fix account banners ui (#320)

--- plugins/simple_kit/lib/src/account/banners/simple_account_banner.dart
+++ plugins/simple_kit/lib/src/account/banners/simple_account_banner.dart
@@ -23,66 +23,65 @@ class SimpleAccountBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.only(
-          right: 10.w,
-        ),
-        width: 327.w,
-        height: 171.h,
-        padding: EdgeInsets.only(
-          left: 20.w,
-          right: 20.w,
-          top: 20.w,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16.r),
-          color: color,
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              radius: 40.r,
-              backgroundImage: setCircleBackgroundImage(imageUrl),
-            ),
-            const SpaceW16(),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 8.h,
-                  ),
-                  child: Baseline(
-                    baseline: 20.h,
-                    baselineType: TextBaseline.alphabetic,
-                    child: Text(
-                      header,
-                      maxLines: 2,
-                      style: sTextH5Style,
+    return Container(
+      margin: const EdgeInsets.only(
+        right: 8,
+      ),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: 327,
+          padding: const EdgeInsets.only(
+            top: 20,
+            left: 20,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.r),
+            color: color,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundImage: setCircleBackgroundImage(imageUrl),
+              ),
+              const SpaceW16(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 14,
                     ),
-                  ),
-                ),
-                SizedBox(
-                  width: 177.w,
-                  child: Baseline(
-                    baseline: 22.h,
-                    baselineType: TextBaseline.alphabetic,
-                    child: Text(
-                      description,
-                      maxLines: 4,
-                      style: sBodyText2Style.copyWith(
-                        color: SColorsLight().grey1,
-                        // height: 1.7.h, //Todo: comeback to this
+                    child: Baseline(
+                      baseline: 20,
+                      baselineType: TextBaseline.alphabetic,
+                      child: Text(
+                        header,
+                        maxLines: 2,
+                        style: sTextH5Style,
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 177,
+                    child: Baseline(
+                      baseline: 22,
+                      baselineType: TextBaseline.alphabetic,
+                      child: Text(
+                        description,
+                        maxLines: 4,
+                        style: sBodyText2Style.copyWith(
+                          color: SColorsLight().grey1,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
