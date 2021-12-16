64d2e04eb62446e7423c098eaf288e10635f92a5
Denis Martych <93989208+DenisMartych@users.noreply.github.com>
2021-12-16 16:55:06 +0200

    SPUI-675 > Fix display about us text (#317)
    * SPUI-675 > Fix display about us text
    
    * SPUI-675 > delete comment code
    
    * SPUI-675 > delete \n in text of about us screen
    
    * SPUI-675 > delete \n in text of about us screen

--- lib/app/shared/features/about_us/view/about_us.dart
+++ lib/app/shared/features/about_us/view/about_us.dart
@@ -17,44 +17,61 @@ class AboutUs extends HookWidget {
         onBackButtonTap: () => Navigator.pop(context),
       ),
       child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          Text(
-            'At Simple, we see \n'
-            'investing differently.',
-            style: sTextH2Style,
+          Baseline(
+            baseline: 60,
+            baselineType: TextBaseline.alphabetic,
+            child: Text(
+              'At Simple, we see investing differently.',
+              maxLines: 3,
+              style: sTextH2Style,
+            ),
           ),
-          const SpaceH20(),
-          Text(
-            'Today’s financial system is complex, \n'
-            'exclusive and expensive - making it hard to \n'
-            'gain access, get lower fees and preferential \n'
-            'treatment. We believe that the financial \n'
-            'system should work for everyone!',
-            style: sBodyText1Style,
+          Baseline(
+            baselineType: TextBaseline.alphabetic,
+            baseline: 48,
+            child: Text(
+              'Today’s financial system is complex, '
+              'exclusive and expensive - making it hard to '
+              'gain access, get lower fees and preferential '
+              'treatment. We believe that the financial '
+              'system should work for everyone!',
+              maxLines: 6,
+              style: sBodyText1Style,
+            ),
           ),
-          const SpaceH20(),
-          Text(
-            'We’re building a product that reimagines \n'
-            'what it means to invest, simplifies the \n'
-            'access and breaks down these complex \n'
-            'barriers and fees. Its an easy to use financial \n'
-            'product that empowers you to see new \n'
-            'possibilities for your money and puts your \n'
-            'money in motion!',
-            style: sBodyText1Style,
+          Baseline(
+            baselineType: TextBaseline.alphabetic,
+            baseline: 48,
+            child: Text(
+              'We’re building a product that reimagines '
+              'what it means to invest, simplifies the '
+              'access and breaks down these complex '
+              'barriers and fees. Its an easy to use financial '
+              'product that empowers you to see new '
+              'possibilities for your money and puts your '
+              'money in motion!',
+              maxLines: 8,
+              style: sBodyText1Style,
+            ),
           ),
-          const SpaceH20(),
-          Text(
-            'We will launch with simple but innovative \n'
-            'cryptocurrency trading and while you are \n'
-            'trading, our team will be working hard to put \n'
-            'the finishing touches on our community \n'
-            'driven trade-everything platform to offer \n'
-            'you stocks, commodities, as well as social \n'
-            'payments. All in one simple to use \n'
-            'application. Its what we call, finance made \n'
-            'SIMPLE.',
-            style: sBodyText1Style,
+          Baseline(
+            baseline: 48,
+            baselineType: TextBaseline.alphabetic,
+            child: Text(
+              'We will launch with simple but innovative '
+              'cryptocurrency trading and while you are '
+              'trading, our team will be working hard to put '
+              'the finishing touches on our community '
+              'driven trade-everything platform to offer '
+              'you stocks, commodities, as well as social '
+              'payments. All in one simple to use '
+              'application. Its what we call, finance made '
+              'SIMPLE.',
+              maxLines: 10,
+              style: sBodyText1Style,
+            ),
           ),
           const SpaceH30(),
           Row(
@@ -74,6 +91,7 @@ class AboutUs extends HookWidget {
               ),
             ],
           ),
+          const SpaceH60(),
         ],
       ),
     );
