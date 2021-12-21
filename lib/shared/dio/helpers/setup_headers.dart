dae254aa2339af153f8763de050196c809e559fb
Eli Stark <elistark.tech@gmail.com>
2021-12-21 12:04:12 +0200

    Add deviceType to signalR

--- lib/shared/dio/helpers/setup_headers.dart
+++ lib/shared/dio/helpers/setup_headers.dart
@@ -12,5 +12,5 @@ void setupHeaders(Dio dio, Reader read, [String? token]) {
   dio.options.headers['Authorization'] = 'Bearer $token';
   dio.options.headers['Accept-Language'] = read(intlPod).localeName;
   dio.options.headers['From'] = read(deviceUidPod);
-  dio.options.headers['User-Agent'] = '${read(appVersionPod)};$deviceType()';
+  dio.options.headers['User-Agent'] = '${read(appVersionPod)};$deviceType';
 }
