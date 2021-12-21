a3d8afd7de68538d9241e50a73302d15b4b1d702
Eli Stark <elistark.tech@gmail.com>
2021-12-21 11:58:21 +0200

    SPUI-822 > Add device type to UserAgent

--- lib/shared/dio/helpers/setup_headers.dart
+++ lib/shared/dio/helpers/setup_headers.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../helpers/device_type.dart';
 import '../../providers/device_uid_pod.dart';
 import '../../providers/package_info_fpod.dart';
 import '../../providers/service_providers.dart';
@@ -11,5 +12,5 @@ void setupHeaders(Dio dio, Reader read, [String? token]) {
   dio.options.headers['Authorization'] = 'Bearer $token';
   dio.options.headers['Accept-Language'] = read(intlPod).localeName;
   dio.options.headers['From'] = read(deviceUidPod);
-  dio.options.headers['User-Agent'] = read(appVersionPod);
+  dio.options.headers['User-Agent'] = '${read(appVersionPod)};$deviceType()';
 }
