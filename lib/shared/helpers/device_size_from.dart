377bc2d5db3f3f6e58c661e63a2919a52576666a
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2021-12-22 18:27:39 +0200

    Fix PR comment - change smallHeightBreakpoint

--- lib/shared/helpers/device_size_from.dart
+++ lib/shared/helpers/device_size_from.dart
@@ -1,12 +1,10 @@
 import '../providers/device_size/device_size_union.dart';
 
-const _smallScreenHeight = 667;
+const _smallHeightBreakpoint = 700;
 
 DeviceSizeUnion deviceSizeFrom(double screenHeight) {
-  if (screenHeight <= _smallScreenHeight) {
+  if (screenHeight <= _smallHeightBreakpoint) {
     return const DeviceSizeUnion.small();
-  } else if (screenHeight >= 667 && screenHeight <= 812) {
-    return const DeviceSizeUnion.medium();
   } else {
     return const DeviceSizeUnion.medium();
   }
