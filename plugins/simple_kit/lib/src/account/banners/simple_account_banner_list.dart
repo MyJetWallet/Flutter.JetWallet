2749efa193e16178381209f212c92d3c8f786436
DenisMartych <denis.m@smplt.net>
2021-12-22 20:12:20 +0200

    SPUI-449 > Add banners scroll to account and market

--- plugins/simple_kit/lib/src/account/banners/simple_account_banner_list.dart
+++ plugins/simple_kit/lib/src/account/banners/simple_account_banner_list.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
+import 'helper/create_banners_list.dart';
 
-import '../../../simple_kit.dart';
-import '../../colors/view/simple_colors_light.dart';
-
-class SimpleAccountBannerList extends StatelessWidget {
-  const SimpleAccountBannerList({
+class SAccountBannerList extends StatelessWidget {
+  const SAccountBannerList({
     Key? key,
     this.onTwoFaBannerTap,
     this.onChatBannerTap,
@@ -21,91 +19,31 @@ class SimpleAccountBannerList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = PageController(viewportFraction: 0.88);
+
+    final banners = createBannersList(
+      twoFaEnabled: twoFaEnabled,
+      kycPassed: kycPassed,
+      phoneVerified: phoneVerified,
+      onChatBannerTap: onChatBannerTap,
+      onTwoFaBannerTap: onTwoFaBannerTap,
+    );
+
     return SizedBox(
-      height: setSizedBoxHeight(
-        kycPassed: kycPassed,
-        phoneVerified: phoneVerified,
-        twoFaEnabled: twoFaEnabled,
-      ),
-      child: Column(
-        children: [
-          if (!kycPassed || !phoneVerified || !twoFaEnabled)
-            Expanded(
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  const SpaceW24(),
-                  const SpaceW8(),
-                  if (!kycPassed)
-                    SimpleAccountBanner(
-                      onTap: () {},
-                      color: SColorsLight().violet,
-                      header: 'Verify your profile',
-                      description: 'In accordance with KYC and AML Policy, '
-                          'you are required to pass the '
-                          'verification process.',
-                    ),
-                  if (!phoneVerified)
-                    SimpleAccountBanner(
-                      onTap: () {},
-                      color: SColorsLight().greenLight,
-                      header: 'Verifying now',
-                      description:
-                          'You’ll be notified after we’ve completed the '
-                              'process. '
-                          'Usually within a few hours',
-                    ),
-                  if (!twoFaEnabled)
-                    SimpleAccountBanner(
-                      onTap: () {
-                        onTwoFaBannerTap?.call();
-                      },
-                      color: SColorsLight().redLight,
-                      header: 'Enable 2-Factor\nauthentication',
-                      description:
-                          'To protect your account, it is recommended '
-                              'to turn on',
-                    ),
-                  SimpleAccountBanner(
-                    onTap: () {
-                      onChatBannerTap?.call();
-                    },
-                    color: SColorsLight().yellowLight,
-                    header: 'Chat with support',
-                    description: 'Have any questions?\nWe here to help 24/7',
-                  ),
-                  const SpaceW14(),
-                ],
-              ),
-            ),
-          if (kycPassed || phoneVerified || twoFaEnabled)
-            Container(
-              alignment: Alignment.center,
-              height: setSizedBoxHeight(
-                kycPassed: kycPassed,
-                phoneVerified: phoneVerified,
-                twoFaEnabled: twoFaEnabled,
-              ),
-              margin: const EdgeInsets.only(left: 8),
-              child: SimpleAccountBanner(
-                onTap: () {
-                  onChatBannerTap?.call();
-                },
-                color: SColorsLight().yellowLight,
-                header: 'Chat with support',
-                description: 'Have any questions?\nWe here to help 24/7',
-              ),
-            ),
-        ],
+      height: _bannerHeight(),
+      child: Expanded(
+        child: PageView.builder(
+          controller: controller,
+          itemCount: banners.length,
+          itemBuilder: (_, index) {
+            return banners[index];
+          },
+        ),
       ),
     );
   }
 
-  double setSizedBoxHeight({
-    required bool kycPassed,
-    required bool phoneVerified,
-    required bool twoFaEnabled,
-  }) {
+  double _bannerHeight() {
     if (!kycPassed || !phoneVerified) {
       return 171;
     } else if (!twoFaEnabled) {
--- plugins/simple_kit/lib/src/banners/rewards_banner/simple_rewards_banner.dart
+++ plugins/simple_kit/lib/src/banners/rewards_banner/simple_rewards_banner.dart
@@ -11,22 +11,29 @@ class SRewardBanner extends StatelessWidget {
     this.imageUrl,
     this.onClose,
     this.primaryTextStyle,
+    this.indentRight = false,
     required this.color,
     required this.primaryText,
   }) : super(key: key);
-
   final Function()? onClose;
   final String? secondaryText;
   final String? imageUrl;
   final TextStyle? primaryTextStyle;
   final Color color;
   final String primaryText;
+  final bool indentRight;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
+        const SpaceW24(),
         Container(
+          margin: indentRight
+              ? EdgeInsets.zero
+              : const EdgeInsets.only(
+            right: 10,
+          ),
           padding: const EdgeInsets.only(
             left: 20.0,
             top: 20.0,
@@ -86,7 +93,7 @@ class SRewardBanner extends StatelessWidget {
         if (onClose != null)
           Positioned(
             top: 12.0,
-            right: 12.0,
+            right: 22.0,
             child: GestureDetector(
               onTap: onClose,
               child: const SEraseMarketIcon(),
