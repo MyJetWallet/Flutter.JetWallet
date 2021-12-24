16d2e685d3e508e3defe69b12d5db1b28168dbac
Eli Stark <elistark.tech@gmail.com>
2021-12-24 14:15:01 +0200

    SPUI-700 > Update startup flow (prod and stage)

--- lib/core/stage/components/app_init.dart
+++ lib/core/stage/components/app_init.dart
@@ -6,19 +6,19 @@ import '../../../app/screens/navigation/view/navigation.dart';
 import '../../../auth/screens/email_verification/view/email_verification.dart';
 import '../../../auth/screens/onboarding/onboarding_screen.dart';
 import '../../../auth/screens/splash/splash_screen.dart';
+import '../../../router/notifier/startup_notifier/startup_notipod.dart';
+import '../../../router/provider/app_init_fpod.dart';
+import '../../../router/provider/authorization_stpod/authorization_stpod.dart';
 import '../../../shared/features/pin_screen/model/pin_flow_union.dart';
 import '../../../shared/features/pin_screen/view/pin_screen.dart';
 import '../../../shared/features/two_fa/two_fa_phone/model/two_fa_phone_trigger_union.dart';
 import '../../../shared/features/two_fa/two_fa_phone/view/two_fa_phone.dart';
-import '../../notifier/startup_notifier/startup_notipod.dart';
-import '../../provider/app_init_fpod.dart';
-import '../../provider/authorization_stpod/authorization_stpod.dart';
 
 /// Launches application goes after [RemoteConfigInit]
 class AppInit extends HookWidget {
   const AppInit({Key? key}) : super(key: key);
 
-  static const routeName = 'app_init';
+  static const routeName = '/app_init';
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +26,7 @@ class AppInit extends HookWidget {
     final appInit = useProvider(appInitFpod);
     final startup = useProvider(startupNotipod);
 
-    return appInit.when(
+    return appInit.maybeWhen(
       data: (_) {
         return router.state.when(
           authorized: () {
@@ -56,8 +56,7 @@ class AppInit extends HookWidget {
           unauthorized: () => const OnboardingScreen(),
         );
       },
-      loading: () => const SplashScreen(),
-      error: (e, st) => Text('$e'),
+      orElse: () => const SplashScreen(),
     );
   }
 }
--- lib/router/view/router.dart
+++ lib/router/view/router.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../app/screens/navigation/view/navigation.dart';
+import '../../auth/screens/email_verification/view/email_verification.dart';
+import '../../auth/screens/onboarding/onboarding_screen.dart';
+import '../../auth/screens/splash/splash_screen.dart';
+import '../../shared/features/pin_screen/model/pin_flow_union.dart';
+import '../../shared/features/pin_screen/view/pin_screen.dart';
+import '../../shared/features/two_fa/two_fa_phone/model/two_fa_phone_trigger_union.dart';
+import '../../shared/features/two_fa/two_fa_phone/view/two_fa_phone.dart';
 import '../../shared/providers/service_providers.dart';
-import 'components/app_init.dart';
-import 'components/remote_config_init.dart';
+import '../provider/router_pod/router_pod.dart';
 
 /// This widget is supposed to be the first one that will
 /// be created at the app launch
@@ -15,10 +22,39 @@ class AppRouter extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final router = useProvider(routerPod);
     useProvider(intlPod.select((_) {}));
 
-    return const RemoteConfigInit(
-      child: AppInit(),
+    return router.when(
+      loading: () {
+        return const SplashScreen();
+      },
+      emailVerification: () {
+        return const EmailVerification();
+      },
+      twoFaVerification: () {
+        return const TwoFaPhone(
+          trigger: TwoFaPhoneTriggerUnion.startup(),
+        );
+      },
+      pinSetup: () {
+        return const PinScreen(
+          union: Setup(),
+          cannotLeave: true,
+        );
+      },
+      pinVerification: () {
+        return const PinScreen(
+          union: Verification(),
+          cannotLeave: true,
+        );
+      },
+      home: () {
+        return Navigation();
+      },
+      unauthorized: () {
+        return const OnboardingScreen();
+      },
     );
   }
 }
