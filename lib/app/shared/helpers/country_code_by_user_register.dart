b5c9ab856e638ad148ec9690816682772a87f71e
DenisMartych <denis.m@smplt.net>
2022-01-21 19:11:27 +0200

    SPUI-923: fix svg

--- lib/app/shared/helpers/country_code_by_user_register.dart
+++ lib/app/shared/helpers/country_code_by_user_register.dart
@@ -3,11 +3,9 @@ import 'package:simple_kit/simple_kit.dart';
 SPhoneNumber? countryCodeByUserRegister(String countryOfRegistration) {
   SPhoneNumber? phoneNumber;
 
-  final countryIsoCodeOfUserRegistration = countryOfRegistration.toLowerCase();
-
   for (var i = 0; i < sPhoneNumbers.length; i++) {
     if (sPhoneNumbers[i].isoCode.toLowerCase() ==
-        countryIsoCodeOfUserRegistration) {
+        countryOfRegistration.toLowerCase()) {
       phoneNumber = sPhoneNumbers[i];
     }
   }
