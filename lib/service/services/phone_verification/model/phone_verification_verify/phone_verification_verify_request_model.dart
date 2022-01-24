f3acd07dfe965881beace459afdf27d93699818c
DenisMartych <denis.m@smplt.net>
2022-01-25 00:45:08 +0200

    SPUI-1024: change contract in phone number, phone_verification, two_fa_phone

--- lib/service/services/phone_verification/model/phone_verification_verify/phone_verification_verify_request_model.dart
+++ lib/service/services/phone_verification/model/phone_verification_verify/phone_verification_verify_request_model.dart
@@ -7,8 +7,6 @@ part 'phone_verification_verify_request_model.g.dart';
 class PhoneVerificationVerifyRequestModel
     with _$PhoneVerificationVerifyRequestModel {
   const factory PhoneVerificationVerifyRequestModel({
-    // required String code,
-    // required String phoneNumber,
     required String phoneBody,
     required String phoneCode,
     required String phoneIso,
