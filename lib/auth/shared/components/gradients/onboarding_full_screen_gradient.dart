421aa4444ad787d6a145684a35cf42d84d81bdb1
Denis Martych <93989208+DenisMartych@users.noreply.github.com>
2022-01-28 19:26:26 +0200

    SPUI-1029/769 > Onboarding (#371)
    * SPUI-769/1029: add image to onboarding and add gestures for change slider
    
    * SPUI-769/1029: delete Spacer()
    
    * SPUI-769: fix restartAnimation when tap in left side of screen
    
    * SPUI-769: refactore sceen, add notifier
    
    * SPUI-1029-769: replace image on 4x
    
    * SPUI-1029-769: update image in small and medium size
    
    * SPUI-1029-769: update ui
    
    Co-authored-by: Eli Stark <elistark.tech@gmail.com>

--- lib/auth/shared/components/gradients/onboarding_full_screen_gradient.dart
+++ lib/auth/shared/components/gradients/onboarding_full_screen_gradient.dart
@@ -5,25 +5,78 @@ import '../styles/gradient_colors.dart';
 class OnboardingFullScreenGradient extends StatelessWidget {
   const OnboardingFullScreenGradient({
     Key? key,
+    this.backgroundColor,
+    this.onTapBack,
+    this.onTapNext,
+    this.onLongPress,
+    this.onLongPressEnd,
+    this.onPanEnd,
     required this.child,
   }) : super(key: key);
 
+  final Function()? onTapBack;
+  final Function()? onTapNext;
+  final Function()? onLongPress;
+  final Function(DragEndDetails)? onPanEnd;
+  final Function(LongPressEndDetails)? onLongPressEnd;
+  final Color? backgroundColor;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 24.0,
+    final size = MediaQuery.of(context).size;
+    
+    return Material(
+      child: Stack(
+        children: [
+          Container(
+            height: size.height,
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            decoration: (backgroundColor != null)
+                ? BoxDecoration(
+                    color: backgroundColor,
+                  )
+                : const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: onBoardingGradientColors,
+                    ),
+                  ),
+            child: child,
+          ),
+          Positioned(
+            top: 0,
+            bottom: size.height / 4.3,
+            left: 0,
+            right: size.width / 2,
+            child: GestureDetector(
+              onTap: onTapBack,
+              onLongPress: onLongPress,
+              onLongPressEnd: onLongPressEnd,
+              onPanEnd: onPanEnd,
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            bottom: size.height / 4.3,
+            right: 0,
+            left: size.width / 2,
+            child: GestureDetector(
+              onTap: onTapNext,
+              onLongPress: onLongPress,
+              onLongPressEnd: onLongPressEnd,
+              onPanEnd: onPanEnd,
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+        ],
       ),
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: onBoardingGradientColors,
-        ),
-      ),
-      child: child,
     );
   }
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -11,6 +11,12 @@ const walletGreenBackgroundImageAsset =
     'assets/images/green_wallet_gradient.svg';
 const walletRedBackgroundImageAsset = 'assets/images/red_wallet_gradient.svg';
 
+// Onboarding images
+const buyCryptoImageAsset = 'assets/images/buy_crypto.png';
+const cryptoIndeciesImageAsset = 'assets/images/crypto_indecies.png';
+const inviteFriendsImageAsset = 'assets/images/invite_friends.png';
+const simpleAppImageAsset = 'assets/images/simple_app.png';
+
 // Kyc popup images
 const verifyYourProfileAsset = 'assets/images/verify_your_profile.png';
 const verifyingNowAsset = 'assets/images/verifying_now.png';
