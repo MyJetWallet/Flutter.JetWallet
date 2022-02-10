9940432d8bc660b3ce5b90adb7971f801d1e51df
Denis Martych <93989208+DenisMartych@users.noreply.github.com>
2022-02-10 10:52:47 +0200

    SPUI-1084 > Clear password on backspace (#410)

--- lib/auth/screens/login/login.dart
+++ lib/auth/screens/login/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:simple_kit/simple_kit.dart';
@@ -107,36 +106,25 @@ class Login extends HookWidget {
                     Material(
                       color: colors.white,
                       child: SPaddingH24(
-                        child: RawKeyboardListener(
-                          focusNode: FocusNode(),
-                          onKey: (event) {
-                            if (event.logicalKey ==
-                                LogicalKeyboardKey.backspace) {
-                              if (passwordError.value.value) {
-                                _controller.clear();
-                                emailError.value.disableError();
-                                passwordError.value.disableError();
-                                credentialsN.updateAndValidatePassword('');
-                              }
-                            }
+                        child: SStandardFieldObscure(
+                          autofillHints: const [AutofillHints.password],
+                          controller: _controller,
+                          onChanged: (String password) {
+                            credentialsN.checkOnUpdateOrRemovePassword(
+                              passwordError,
+                              emailError,
+                              password,
+                              _controller,
+                            );
                           },
-                          child: SStandardFieldObscure(
-                            autofillHints: const [AutofillHints.password],
-                            controller: _controller,
-                            onChanged: (String password) {
-                              emailError.value.disableError();
-                              passwordError.value.disableError();
-                              credentialsN.updateAndValidatePassword(password);
-                            },
-                            labelText: intl.login_passwordTextFieldLabel,
-                            onErrorIconTap: () {
-                              sShowErrorNotification(
-                                notificationQueueN,
-                                intl.login_credentialsError,
-                              );
-                            },
-                            errorNotifier: passwordError.value,
-                          ),
+                          labelText: intl.login_passwordTextFieldLabel,
+                          onErrorIconTap: () {
+                            sShowErrorNotification(
+                              notificationQueueN,
+                              intl.login_credentialsError,
+                            );
+                          },
+                          errorNotifier: passwordError.value,
                         ),
                       ),
                     ),
