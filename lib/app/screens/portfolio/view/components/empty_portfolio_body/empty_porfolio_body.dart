466ca5d2ff87216f7625139fef6740356bd2de51
DenisMartych <denis.m@smplt.net>
2022-02-10 18:48:27 +0200

    SPUI-1132 > Fix portfolio ui

--- lib/app/screens/portfolio/view/components/empty_portfolio_body/empty_porfolio_body.dart
+++ lib/app/screens/portfolio/view/components/empty_portfolio_body/empty_porfolio_body.dart
@@ -42,9 +42,9 @@ class EmptyPortfolioBody extends HookWidget {
                 ),
                 Column(
                   children: [
-                    const SpaceH36(),
+                    const Spacer(),
                     const EmptyPortfolioBodyImage(),
-                    const SpaceH56(),
+                    const Spacer(),
                     const EmptyPortfolioBodyTitle(),
                     const SpaceH17(),
                     Text(
