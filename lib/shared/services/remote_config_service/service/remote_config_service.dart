2c28dc96b049e5215fca6a5d911236e9628b633a
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2022-02-14 22:04:34 +0200

    Fix receiving of remote config support values

--- lib/shared/services/remote_config_service/service/remote_config_service.dart
+++ lib/shared/services/remote_config_service/service/remote_config_service.dart
@@ -25,6 +25,7 @@ class RemoteConfigService {
     overrideAppConfigValues();
     overrideApisFrom(_defaultFlavorIndex);
     overrideVersioningValues();
+    overrideSupportValues();
   }
 
   ConnectionFlavorsModel get connectionFlavors {
