146f6001f95180c855d249f0850c1b1ea960d876
Volodymyr Medvediev <vovkamedvedev5@gmail.com>
2022-02-18 13:44:08 +0200

    Bring changes back

--- lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_list_item_header_text.dart
+++ lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_list_item_header_text.dart
@@ -4,21 +4,21 @@ import 'package:simple_kit/simple_kit.dart';
 class TransactionListItemHeaderText extends StatelessWidget {
   const TransactionListItemHeaderText({
     Key? key,
-    this.textAlign,
+    required this.width,
     required this.text,
   }) : super(key: key);
 
   final String text;
-  final TextAlign? textAlign;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: sSubtitle2Style,
-      maxLines: 1,
-      overflow: TextOverflow.ellipsis,
-      textAlign: textAlign,
+    return SizedBox(
+      width: width,
+      child: Text(
+        text,
+        style: sSubtitle2Style,
+      ),
     );
   }
 }
