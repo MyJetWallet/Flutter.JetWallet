cf19dbcb07ce0a5d01bf35eb1c42cd47db428c3d
Vladimir <vovkamedvedev5@gmail.com>
2022-02-21 12:54:23 +0200

    SPUI-1127 > Cut transaction name in list item (#434)
    * Cut transaction name in list item
    
    * Refactor cutting of transaction name and volume
    
    * Bring changes back
    
    * Add todo
    
    * Bring textSize function back to wallet_card widget
    
    * Fix cutting of transaction name and volume

--- lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_list_item_header_text.dart
+++ lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_list_item_header_text.dart
@@ -4,16 +4,21 @@ import 'package:simple_kit/simple_kit.dart';
 class TransactionListItemHeaderText extends StatelessWidget {
   const TransactionListItemHeaderText({
     Key? key,
+    this.textAlign,
     required this.text,
   }) : super(key: key);
 
   final String text;
+  final TextAlign? textAlign;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       style: sSubtitle2Style,
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      textAlign: textAlign,
     );
   }
 }
