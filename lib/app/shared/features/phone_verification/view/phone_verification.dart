6ba8cd64e928c9d955447bb770b417b4d36c86a4
DenisMartych <denis.m@smplt.net>
2022-03-09 13:49:38 +0200

    SPUI-1184 > Add support button in phone confirmation

--- lib/app/shared/features/phone_verification/view/phone_verification.dart
+++ lib/app/shared/features/phone_verification/view/phone_verification.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -11,6 +12,7 @@ import '../../../../../shared/helpers/navigator_push.dart';
 import '../../../../../shared/helpers/navigator_push_replacement.dart';
 import '../../../../../shared/notifiers/timer_notifier/timer_notipod.dart';
 import '../../../../../shared/services/remote_config_service/remote_config_values.dart';
+import '../../../../screens/account/components/crisp.dart';
 import '../notifier/phone_verification_notipod.dart';
 
 class PhoneVerificationArgs {
@@ -102,6 +104,9 @@ class PhoneVerification extends HookWidget {
                     ),
                     TextSpan(
                       text: 'support',
+                      recognizer: TapGestureRecognizer()..onTap = () {
+                        Crisp.push(context);
+                      },
                       style: sBodyText1Style.copyWith(
                         color: colors.blue,
                       ),
