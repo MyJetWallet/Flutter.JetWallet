ace98c52db920738ddcb47edd15762c1b39878fa
Eli Stark <elistark.tech@gmail.com>
2022-03-10 12:14:07 +0200

    SPUI-1198 > Payment methods for circle (#466)
    * update provider ignore
    
    * update provider ignore
    
    * add todo
    
    * add transport layer skeleton for circle
    
    * finish circle service
    
    * Add ui for payment methods
    
    * update
    
    * fix opengp and fast_rsa conflict
    
    * update payment methods ui
    
    * update
    
    * update

---
+++ lib/app/shared/features/enter_card_details/view/enter_card_details.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:simple_kit/simple_kit.dart';
+
+import '../../../../../shared/helpers/navigator_push.dart';
+import '../notifier/enter_card_details_notipod.dart';
+import 'components/country_selector_button.dart';
+import 'components/show_country_selector.dart';
+
+class EnterCardDetails extends HookWidget {
+  const EnterCardDetails({Key? key}) : super(key: key);
+
+  static void push(BuildContext context) {
+    navigatorPush(context, const EnterCardDetails());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = useProvider(sColorPod);
+    final state = useProvider(enterCardDetailsNotipod);
+    final notifier = useProvider(enterCardDetailsNotipod.notifier);
+
+    return SPageFrame(
+      header: SPaddingH24(
+        child: SBigHeader(
+          customIconButton: SIconButton(
+            onTap: () => Navigator.pop(context),
+            defaultIcon: const SCloseIcon(),
+            pressedIcon: const SClosePressedIcon(),
+          ),
+          title: 'Enter card details',
+        ),
+      ),
+      child: Stack(
+        children: [
+          ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'Card number',
+                ),
+              ),
+              Row(
+                children: const [
+                  Expanded(
+                    child: SFieldDividerFrame(
+                      child: SStandardField(
+                        labelText: 'Expiry Date',
+                      ),
+                    ),
+                  ),
+                  SDivider(
+                    width: 1.0,
+                    height: 88.0,
+                  ),
+                  Expanded(
+                    child: SFieldDividerFrame(
+                      child: SStandardField(
+                        labelText: 'CVV',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'Card name',
+                ),
+              ),
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'Cardholder name',
+                ),
+              ),
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'Street Address',
+                ),
+              ),
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'Street Address 2 (optional)',
+                ),
+              ),
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'City',
+                ),
+              ),
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'District',
+                ),
+              ),
+              const SFieldDividerFrame(
+                child: SStandardField(
+                  labelText: 'Postal code',
+                ),
+              ),
+              CountrySelectorButton(
+                country: state.selectedCountry!,
+                onTap: () => showCountrySelector(context),
+              ),
+              const SpaceH136()
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Material(
+              color: colors.grey5,
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: SPrimaryButton2(
+                  active: true,
+                  name: 'Continue',
+                  onTap: () {
+                    notifier.addCard();
+                  },
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
