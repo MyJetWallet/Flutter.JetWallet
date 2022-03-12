8e9e5d32c0f6a8b9ca49ddc61da725c9ebd09170
DenisMartych <denis.m@smplt.net>
2022-03-12 11:37:27 +0200

    SPUI-1139 > Update placeholder

--- lib/app/screens/market/view/components/market_tab_bar_views/components/empty_watchlist.dart
+++ lib/app/screens/market/view/components/market_tab_bar_views/components/empty_watchlist.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:simple_kit/simple_kit.dart';
 
