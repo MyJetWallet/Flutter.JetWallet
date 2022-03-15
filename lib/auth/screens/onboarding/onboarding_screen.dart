6a7dbd2844465c5da6c1135085fb459f8ad0ca27
Denis Martych <93989208+DenisMartych@users.noreply.github.com>
2022-03-15 15:48:36 +0200

    SPUI-1136 > Remove text animation on onboarding (#472)

--- lib/auth/screens/onboarding/onboarding_screen.dart
+++ lib/auth/screens/onboarding/onboarding_screen.dart
@@ -13,7 +13,6 @@ import '../login/login.dart';
 import '../register/register.dart';
 import 'components/animated_slide.dart';
 
-const _textAnimationDuration = Duration(seconds: 2);
 const _slidesAnimationDuration = Duration(seconds: 4);
 
 const onboardingImages = [
@@ -32,26 +31,17 @@ class OnboardingScreen extends StatefulHookWidget {
 
 class _OnBoardingScreenState extends State<OnboardingScreen>
     with TickerProviderStateMixin {
-  late final AnimationController _textAnimationController = AnimationController(
-    vsync: this,
-  );
-  late final Animation<double> _textAnimation = CurvedAnimation(
-    parent: _textAnimationController,
-    curve: Curves.easeIn,
-  );
   late final AnimationController _slidesAnimationController =
       AnimationController(
     vsync: this,
   );
   int _currentIndex = 0;
-  bool _reverse = false;
 
   late List<String> _slides;
 
   @override
   void initState() {
     super.initState();
-    _restartTextAnimation();
     _restartAnimation();
 
     final intl = context.read(intlPod);
@@ -63,17 +53,6 @@ class _OnBoardingScreenState extends State<OnboardingScreen>
       intl.onboarding_slide4,
     ];
 
-    _textAnimationController.addStatusListener((status) {
-      if (status == AnimationStatus.completed ||
-          status == AnimationStatus.dismissed) {
-        if (_currentIndex < _slides.length) {
-          setState(() {
-            _reverse = !_reverse;
-            _restartTextAnimation();
-          });
-        }
-      }
-    });
     _slidesAnimationController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         setState(() {
@@ -135,38 +114,31 @@ class _OnBoardingScreenState extends State<OnboardingScreen>
                       return const SpaceH40();
                     },
                   ),
-                  FadeTransition(
-                    opacity: _textAnimation,
-                    child: Column(
-                      children: [
-                        deviceSize.when(
-                          small: () {
-                            return Baseline(
-                              baselineType: TextBaseline.alphabetic,
-                              baseline: 38,
-                              child: Text(
-                                _slides[_currentIndex],
-                                maxLines: 3,
-                                textAlign: TextAlign.center,
-                                style: sTextH2Style,
-                              ),
-                            );
-                          },
-                          medium: () {
-                            return Baseline(
-                              baselineType: TextBaseline.alphabetic,
-                              baseline: 38,
-                              child: Text(
-                                _slides[_currentIndex],
-                                maxLines: 3,
-                                textAlign: TextAlign.center,
-                                style: sTextH1Style,
-                              ),
-                            );
-                          },
+                  deviceSize.when(
+                    small: () {
+                      return Baseline(
+                        baselineType: TextBaseline.alphabetic,
+                        baseline: 38,
+                        child: Text(
+                          _slides[_currentIndex],
+                          maxLines: 3,
+                          textAlign: TextAlign.center,
+                          style: sTextH2Style,
                         ),
-                      ],
-                    ),
+                      );
+                    },
+                    medium: () {
+                      return Baseline(
+                        baselineType: TextBaseline.alphabetic,
+                        baseline: 38,
+                        child: Text(
+                          _slides[_currentIndex],
+                          maxLines: 3,
+                          textAlign: TextAlign.center,
+                          style: sTextH1Style,
+                        ),
+                      );
+                    },
                   ),
                   const Spacer(),
                   deviceSize.when(
@@ -207,7 +179,6 @@ class _OnBoardingScreenState extends State<OnboardingScreen>
 
   @override
   void dispose() {
-    _textAnimationController.dispose();
     _slidesAnimationController.dispose();
     super.dispose();
   }
@@ -217,19 +188,6 @@ class _OnBoardingScreenState extends State<OnboardingScreen>
     _slidesAnimationController.reset();
     _slidesAnimationController.duration = _slidesAnimationDuration;
     _slidesAnimationController.forward();
-  }
-
-  void _restartTextAnimation() {
-    if (_reverse) {
-      if (_currentIndex + 1 != _slides.length) {
-        _textAnimationController.reverse();
-      }
-    } else {
-      _textAnimationController.stop();
-      _textAnimationController.reset();
-      _textAnimationController.duration = _textAnimationDuration;
-      _textAnimationController.forward();
-    }
   }
 
   void _onPanEnd(DragEndDetails details) {
