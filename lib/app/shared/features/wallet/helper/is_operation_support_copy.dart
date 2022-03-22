b8f3e04c89743fac3cae7cf7c73b3d7ee7023d78
Denis Martych <93989208+DenisMartych@users.noreply.github.com>
2022-03-22 09:59:05 +0200

    SPUI-1218 > Fix copied in transaction history (#498)
    * SPUI-1218 > fix copied in transaction history
    
    * SPUI-1218 > fix copied in transaction history
    
    * SPUI-1218 > fix git comments
    
    * fix
    
    * fix
    
    * fix
    
    Co-authored-by: Eli Stark <elistark.tech@gmail.com>

---
+++ lib/app/shared/features/wallet/helper/is_operation_support_copy.dart
@@ -0,0 +1,7 @@
+import '../../../../../service/services/operation_history/model/operation_history_response_model.dart';
+
+bool isOperationSupportCopy(OperationHistoryItem item) {
+  return item.operationType != OperationType.paidInterestRate &&
+      item.operationType != OperationType.rewardPayment &&
+      item.operationType != OperationType.receiveByPhone;
+}
