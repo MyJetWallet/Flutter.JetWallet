696fe7423789c4c94ad1f570f7f6cec93f400825
DenisMartych <denis.m@smplt.net>
2022-03-29 14:15:25 +0300

    SPUI-719 > fix news comments from git

--- lib/app/screens/news/notifier/news_notipod.dart
+++ lib/app/screens/news/notifier/news_notipod.dart
@@ -3,8 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'news_notifier.dart';
 import 'news_state.dart';
 
-final newsNotipod =
-    StateNotifierProvider.autoDispose<NewsNotifier, NewsState>(
+final newsNotipod = StateNotifierProvider.autoDispose<NewsNotifier, NewsState>(
   (ref) {
     return NewsNotifier(
       read: ref.read,
