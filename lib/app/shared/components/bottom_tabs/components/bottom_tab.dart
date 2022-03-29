72d8fdc7b0d5e66ecefb3d83a9388641486c5c94
Anton-Tesenchuk <98030035+Anton-Tesenchuk@users.noreply.github.com>
2022-03-29 16:28:50 +0300

    SPUI-776 > Market tabs border (#513)
    * WIP market borders
    
    * SPUI-776 Look like finished
    
    * SPUI-776 Fix double bordered
    
    * SPUI-776 Fix build error
    
    * SPUI-776 > Fix comment to pr
    
    Co-authored-by: Anton_Tesenchuk <anton.tesenchuk@simplbit.com>

--- lib/app/shared/components/bottom_tabs/components/bottom_tab.dart
+++ lib/app/shared/components/bottom_tabs/components/bottom_tab.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:simple_kit/simple_kit.dart';
 
-class BottomTab extends StatelessWidget {
+class BottomTab extends HookWidget {
   const BottomTab({
     Key? key,
     required this.text,
@@ -10,13 +13,26 @@ class BottomTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = useProvider(sColorPod);
+
     return Container(
       margin: const EdgeInsets.only(
         right: 10,
+        top: 1,
       ),
-      padding: const EdgeInsets.symmetric(
-        vertical: 6,
-        horizontal: 16,
+      padding: const EdgeInsets.only(
+        top: 3,
+        bottom: 7,
+        right: 15,
+        left: 15,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(24),
+        ),
+        border: Border.all(
+          color: colors.black.withOpacity(0.3),
+        ),
       ),
       child: Text(text),
     );
