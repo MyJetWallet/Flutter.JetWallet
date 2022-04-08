f40a66133467d88f3c799e46579f87d5684c8541
DenisMartych <denis.m@smplt.net>
2022-04-08 18:35:53 +0300

    SPUI-1372 > add status to simplex transaction history

--- lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_details/buy_simplex_details.dart
+++ lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_details/buy_simplex_details.dart
@@ -1,14 +1,10 @@
 import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:simple_kit/simple_kit.dart';
 
 import '../../../../../../../../../../../service/services/operation_history/model/operation_history_response_model.dart';
-import '../../../../../../../../../helpers/formatting/formatting.dart';
 import '../../../../../../../../../helpers/short_address_form.dart';
-import '../../../../../../../../../providers/currencies_pod/currencies_pod.dart';
-import '../../../../../../../../market_details/helper/currency_from.dart';
 import 'components/transaction_details_item.dart';
 import 'components/transaction_details_status.dart';
 import 'components/transaction_details_value_text.dart';
@@ -25,13 +21,6 @@ class BuySimplexDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currencies = context.read(currenciesPod);
-
-    final sellSimplexCurrency = currencyFrom(
-      currencies,
-      transactionListItem.buyInfo!.sellAssetId,
-    );
-
     return SPaddingH24(
       child: Column(
         children: [
@@ -63,12 +52,7 @@ class BuySimplexDetails extends StatelessWidget {
           TransactionDetailsItem(
             text: 'With',
             value: TransactionDetailsValueText(
-              text: volumeFormat(
-                prefix: sellSimplexCurrency.prefixSymbol,
-                decimal: transactionListItem.buyInfo!.sellAmount,
-                accuracy: sellSimplexCurrency.accuracy,
-                symbol: sellSimplexCurrency.symbol,
-              ),
+              text: '\$${transactionListItem.buyInfo!.sellAmount}',
             ),
           ),
           const SpaceH14(),
--- lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_details/components/common_transaction_details_block.dart
+++ lib/app/shared/features/wallet/view/components/wallet_body/components/transactions_list_item/components/transaction_details/components/common_transaction_details_block.dart
@@ -2,12 +2,12 @@ import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:simple_kit/simple_kit.dart';
 
 import '../../../../../../../../../../../../service/services/operation_history/model/operation_history_response_model.dart';
 import '../../../../../../../../../../providers/currencies_pod/currencies_pod.dart';
 import '../../../../../../../../../market_details/helper/currency_from.dart';
+import '../../../../../../../../helper/format_date_to_hm.dart';
 import '../../../../../../../../helper/is_operation_support_copy.dart';
 import '../../../../../../../../helper/operation_name.dart';
 
@@ -58,9 +58,8 @@ class CommonTransactionDetailsBlock extends HookWidget {
             ),
           ),
         Text(
-          DateFormat('EEEE, MMMM d, y').format(
-            DateTime.parse('${transactionListItem.timeStamp}Z').toLocal(),
-          ),
+          '${formatDateToDMY(transactionListItem.timeStamp)} '
+          '- ${formatDateToHm(transactionListItem.timeStamp)}',
           style: sBodyText2Style.copyWith(
             color: colors.grey2,
           ),
