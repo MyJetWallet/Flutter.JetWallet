7c7e7cc2d56701e734a4a4b4fcdd33e2c82a8754
DenisMartych <denis.m@smplt.net>
2022-04-13 14:24:35 +0300

    SPUI-1404 > Recurring banner

---
+++ plugins/simple_kit/lib/src/banners/market_banner/simple_smallest_banner.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../../../simple_kit.dart';
+import '../rewards_banner/helper/set_circle_background_image.dart';
+
+class SSmallestBanner extends StatelessWidget {
+  const SSmallestBanner({
+    Key? key,
+    this.imageUrl,
+    required this.color,
+    required this.primaryText,
+  }) : super(key: key);
+
+  final String? imageUrl;
+  final Color color;
+  final String primaryText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      width: 327,
+      height: 68,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 10,
+              bottom: 10,
+              left: 53,
+            ),
+            child: CircleAvatar(
+              radius: 24.0,
+              backgroundImage: setCircleBackgroundImage(
+                imageUrl,
+              ),
+            ),
+          ),
+          const SpaceW20(),
+          SizedBox(
+            width: 146,
+            child: Text(
+              primaryText,
+              maxLines: 2,
+              style: sSubtitle3Style,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
