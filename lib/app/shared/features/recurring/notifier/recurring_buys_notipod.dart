ab8aa09bda5b921699aec5ce869cdaf9e0becbd8
DenisMartych <denis.m@smplt.net>
2022-04-18 20:34:28 +0300

    SPUI-1406 > fix analyzer

--- lib/app/shared/features/recurring/notifier/recurring_buys_notipod.dart
+++ lib/app/shared/features/recurring/notifier/recurring_buys_notipod.dart
@@ -4,8 +4,8 @@ import '../provider/recurring_buys_pod.dart';
 import 'recurring_buys_notifier.dart';
 import 'recurring_buys_state.dart';
 
-final recurringBuysNotipod =
-    StateNotifierProvider.autoDispose<RecurringBuysNotifier, RecurringBuysState>(
+final recurringBuysNotipod = StateNotifierProvider.autoDispose<
+    RecurringBuysNotifier, RecurringBuysState>(
   (ref) {
     final recurringBuys = ref.watch(recurringBuysPod);
 
