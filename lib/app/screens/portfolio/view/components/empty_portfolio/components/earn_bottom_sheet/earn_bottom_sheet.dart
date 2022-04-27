de3d36a50e9122ed9ac9b34c655715aa23967340
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2022-04-27 13:30:40 +0300

    SPUI-1429 > WiP

--- lib/app/screens/portfolio/view/components/empty_portfolio/components/earn_bottom_sheet/earn_bottom_sheet.dart
+++ lib/app/screens/portfolio/view/components/empty_portfolio/components/earn_bottom_sheet/earn_bottom_sheet.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:simple_kit/simple_kit.dart';
 
 import '../../../../../../../shared/models/currency_model.dart';
+import '../../../../../../market/view/components/fade_on_scroll.dart';
 import 'components/earn_body.dart';
 import 'components/earn_pinned.dart';
 import 'components/earn_pinned_small.dart';
@@ -10,6 +13,7 @@ void showStartEarnBottomSheet({
   required BuildContext context,
   required Function(CurrencyModel) onTap,
 }) {
+  final controller = useScrollController();
   sShowBasicModalBottomSheet(
     context: context,
     removePinnedPadding: true,
@@ -17,8 +21,23 @@ void showStartEarnBottomSheet({
     removeBarPadding: true,
     horizontalPinnedPadding: 0,
     scrollable: true,
+    pinned: SliverAppBar(
+      pinned: true,
+      elevation: 0,
+      expandedHeight: 160,
+      collapsedHeight: 120,
+      primary: false,
+      flexibleSpace: FadeOnScroll(
+        scrollController: controller,
+        fullOpacityOffset: 50,
+        fadeInWidget: const Text('123'),
+        fadeOutWidget: const Text('123324'),
+        permanentWidget: const SMarketHeaderClosed(
+          title: 'Market',
+        ),
+      ),
+    ),
     children: [
-      const EarnPinned(),
       EarnBody(
         onTap: onTap,
       ),
