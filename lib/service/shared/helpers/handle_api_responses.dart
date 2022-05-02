c4ad8c5d041e73ccc30b77a57ef40ba380deb6be
Eli Stark <elistark.tech@gmail.com>
2022-05-02 02:25:02 -0700

    SPUI-1462 > Display remaining time for unblock on sign in

--- lib/service/shared/helpers/handle_api_responses.dart
+++ lib/service/shared/helpers/handle_api_responses.dart
@@ -7,7 +7,7 @@ import 'error_codes_description.dart';
 Map<String, dynamic> handleFullResponse<T>(Map<String, dynamic> json) {
   final result = json['result'] as String;
 
-  _validateResultResponse(result);
+  _validateFullResponse(result, json);
 
   final data = json['data'];
 
@@ -25,8 +25,51 @@ void handleResultResponse(Map<String, dynamic> json) {
   _validateResultResponse(result);
 }
 
+void _validateFullResponse(String result, Map<String, dynamic> json) {
+  if (result == 'OperationBlocked') {
+    final data = json['data'] as Map<String, dynamic>;
+    final blockerExpired = data['blockerExpired'] as String;
+
+    throw ServerRejectException(
+      'Access to account was restricted, '
+      'remaining time is ${_timeLeft(blockerExpired)}.',
+    );
+  } else if (result != 'OK') {
+    throw ServerRejectException(errorCodesDescription[result] ?? result);
+  }
+}
+
 void _validateResultResponse(String result) {
   if (result != 'OK') {
     throw ServerRejectException(errorCodesDescription[result] ?? result);
+  }
+}
+
+String _timeLeft(String blockerExpired) {
+  final split = blockerExpired.split(':');
+  var hours = split[0];
+  final minutes = split[1];
+  var seconds = split[2];
+
+  if (hours[0] == '-') {
+    hours = hours.substring(1);
+  }
+
+  seconds = seconds.substring(0, 2);
+
+  final hInt = int.parse(hours);
+  final mInt = int.parse(minutes);
+  final sInt = int.parse(seconds);
+
+  final hEnd = hInt == 1 ? '' : 's';
+  final mEnd = mInt == 1 ? '' : 's';
+  final sEnd = sInt == 1 ? '' : 's';
+
+  if (hInt != 0) {
+    return '$hInt hour$hEnd $mInt minute$mEnd $sInt second$sEnd';
+  } else if (mInt != 0) {
+    return '$mInt minute$mEnd $sInt second$sEnd';
+  } else {
+    return '$sInt second$sEnd';
   }
 }
