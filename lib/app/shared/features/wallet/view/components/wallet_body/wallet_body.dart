ac7c0cfcefe6b14f452cc0a979f0fd72e510f7e2
Vladimir <vovkamedvedev5@gmail.com>
2022-05-03 13:05:09 +0300

    SPUI-1342 > Fix transaction list error state (#645)
    * Fix transaction list error state
    
    * Fix linter

--- lib/app/shared/features/wallet/view/components/wallet_body/wallet_body.dart
+++ lib/app/shared/features/wallet/view/components/wallet_body/wallet_body.dart
@@ -169,7 +169,6 @@ class _WalletBodyState extends State<WalletBody>
             ),
             TransactionsList(
               scrollController: _scrollController,
-              errorBoxPaddingMultiplier: 0.7133,
               symbol: widget.currency.symbol,
             ),
           ],
