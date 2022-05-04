55e2047dff6d30697c2a5040b5916a94cc801c61
DenisMartych <denis.m@smplt.net>
2022-05-04 16:58:13 +0300

    SPUI-1455 > Add disclaimer ui fix

--- lib/app/shared/features/disclaimer/view/components/disclaimer_popup.dart
+++ lib/app/shared/features/disclaimer/view/components/disclaimer_popup.dart
@@ -3,82 +3,111 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:simple_kit/simple_kit.dart';
 
 import '../../model/disclaimer_model.dart';
+import '../../notifier/disclaimer_notipod.dart';
+
+// void sShowDisclaimerPopup(
+//   BuildContext context, {
+//   Function()? onWillPop,
+//   Function()? onSecondaryButtonTap,
+//   String? secondaryText,
+//   String? secondaryButtonName,
+//   Widget? topSpacer,
+//   Widget? child,
+//   bool willPopScope = true,
+//   bool activePrimaryButton = true,
+//   SButtonType primaryButtonType = SButtonType.primary1,
+//   required List<DisclaimerQuestionsModel> questions,
+//   required Widget image,
+//   required String primaryText,
+//   required String primaryButtonName,
+//   required Widget button,
+// }) {
+//   final colors = context.read(sColorPod);
+//
+//   showDialog(
+//     context: context,
+//     barrierDismissible: false,
+//     builder: (context) {
+//       return WillPopScope(
+//         onWillPop: () {
+//           return Future.value(false);
+//         },
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.end,
+//           children: [
+//             Dialog(
+//               insetPadding: const EdgeInsets.all(24.0),
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(24.0),
+//               ),
+//               child: Stack(
+//                 children: [
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(
+//                       horizontal: 20.0,
+//                     ),
+//                     child: SingleChildScrollView(
+//                       physics: const AlwaysScrollableScrollPhysics(),
+//                       child: Column(
+//                         children: [
+//                           topSpacer ?? const SpaceH40(),
+//                           image,
+//                           Baseline(
+//                             baseline: 40.0,
+//                             baselineType: TextBaseline.alphabetic,
+//                             child: Text(
+//                               primaryText,
+//                               maxLines: (secondaryText != null) ? 5 : 12,
+//                               textAlign: TextAlign.center,
+//                               style: sTextH5Style.copyWith(
+//                                 overflow: TextOverflow.visible,
+//                               ),
+//                             ),
+//                           ),
+//                           const SpaceH7(),
+//                           if (secondaryText != null)
+//                             Text(
+//                               secondaryText,
+//                               maxLines: 6,
+//                               textAlign: TextAlign.center,
+//                               style: sBodyText1Style.copyWith(
+//                                 color: colors.grey1,
+//                               ),
+//                             ),
+//                           const SpaceH40(),
+//                           const SDivider(),
+//                           if (child != null) child,
+//                           const SpaceH20(),
+//                         ],
+//                       ),
+//                     ),
+//                   ),
+//                   button,
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       );
+//     },
+//   );
+// }
+
+
+
+
+
 
 void sShowDisclaimerPopup(
-  BuildContext context, {
-  Function()? onWillPop,
-  Function()? onSecondaryButtonTap,
-  String? secondaryText,
-  String? secondaryButtonName,
-  Widget? topSpacer,
-  Widget? child,
-  bool willPopScope = true,
-  bool activePrimaryButton = true,
-  SButtonType primaryButtonType = SButtonType.primary1,
-  required List<DisclaimerQuestionsModel> questions,
-  required Widget image,
-  required String primaryText,
-  required String primaryButtonName,
-}) {
-  final colors = context.read(sColorPod);
+    BuildContext context, {
+      required Widget child,
+    }) {
 
   showDialog(
     context: context,
     barrierDismissible: false,
     builder: (context) {
-      return WillPopScope(
-        onWillPop: () {
-          return Future.value(false);
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Dialog(
-              insetPadding: const EdgeInsets.all(24.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20.0,
-                ),
-                child: Column(
-                  children: [
-                    topSpacer ?? const SpaceH40(),
-                    image,
-                    Baseline(
-                      baseline: 40.0,
-                      baselineType: TextBaseline.alphabetic,
-                      child: Text(
-                        primaryText,
-                        maxLines: (secondaryText != null) ? 5 : 12,
-                        textAlign: TextAlign.center,
-                        style: sTextH5Style.copyWith(
-                          overflow: TextOverflow.visible,
-                        ),
-                      ),
-                    ),
-                    const SpaceH7(),
-                    if (secondaryText != null)
-                      Text(
-                        secondaryText,
-                        maxLines: 6,
-                        textAlign: TextAlign.center,
-                        style: sBodyText1Style.copyWith(
-                          color: colors.grey1,
-                        ),
-                      ),
-                    const SpaceH40(),
-                    const SDivider(),
-                    if (child != null) child,
-                    const SpaceH20(),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
+      return child;
     },
   );
 }
--- lib/app/shared/features/disclaimer/view/disclaimer.dart
+++ lib/app/shared/features/disclaimer/view/disclaimer.dart
@@ -6,40 +6,53 @@ import '../model/disclaimer_model.dart';
 import '../notifier/disclaimer_notipod.dart';
 import 'components/disclaimer_popup.dart';
 
-void showsDisclaimer({
-  String? imageAsset,
-  String? secondaryButtonName,
-  Function()? onSecondaryButtonTap,
-  Widget? child,
-  bool activePrimaryButton = true,
-  required List<DisclaimerQuestionsModel> questions,
-  required BuildContext context,
-  required String primaryText,
-  required String secondaryText,
-  required String primaryButtonName,
-}) {
-  final state = context.read(disclaimerNotipod);
+// void showsDisclaimer({
+//   String? imageAsset,
+//   String? secondaryButtonName,
+//   Function()? onSecondaryButtonTap,
+//   Widget? child,
+//   bool activePrimaryButton = true,
+//   required List<DisclaimerQuestionsModel> questions,
+//   required BuildContext context,
+//   required String primaryText,
+//   required String secondaryText,
+//   required String primaryButtonName,
+//   required Widget button,
+// }) {
+//   final state = context.read(disclaimerNotipod);
+//
+//   sShowDisclaimerPopup(
+//     context,
+//     image: SizedBox(
+//       height: 80,
+//       width: 80,
+//       child: imageAsset != null
+//           ? Image.network(
+//               imageAsset,
+//             )
+//           : Image.asset(
+//               disclaimerAsset,
+//             ),
+//     ),
+//     primaryText: primaryText,
+//     secondaryText: secondaryText,
+//     child: child,
+//     primaryButtonName: primaryButtonName,
+//     secondaryButtonName: secondaryButtonName,
+//     onSecondaryButtonTap: onSecondaryButtonTap,
+//     activePrimaryButton: state.activeButton,
+//     questions: questions,
+//     button: button,
+//   );
+// }
 
+
+void showsDisclaimer({
+  required Widget child,
+  required BuildContext context,
+}) {
   sShowDisclaimerPopup(
     context,
-    image: SizedBox(
-      height: 80,
-      width: 80,
-      child: imageAsset != null
-          ? Image.network(
-              imageAsset,
-            )
-          : Image.asset(
-              disclaimerAsset,
-            ),
-    ),
-    primaryText: primaryText,
-    secondaryText: secondaryText,
     child: child,
-    primaryButtonName: primaryButtonName,
-    secondaryButtonName: secondaryButtonName,
-    onSecondaryButtonTap: onSecondaryButtonTap,
-    activePrimaryButton: state.activeButton,
-    questions: questions,
   );
 }
