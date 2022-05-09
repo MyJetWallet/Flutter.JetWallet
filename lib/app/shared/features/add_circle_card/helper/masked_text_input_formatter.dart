01d324ea1030dd347490304a2e93ecc9d48ca520
Eli Stark <elistark.tech@gmail.com>
2022-05-09 10:54:48 +0300

    SPUI-1497 > Fix formatting of the card number in circle feature (#677)

--- lib/app/shared/features/add_circle_card/helper/masked_text_input_formatter.dart
+++ lib/app/shared/features/add_circle_card/helper/masked_text_input_formatter.dart
@@ -14,21 +14,65 @@ class MaskedTextInputFormatter extends TextInputFormatter {
     TextEditingValue oldValue,
     TextEditingValue newValue,
   ) {
-    if (newValue.text.isNotEmpty) {
-      if (newValue.text.length > oldValue.text.length) {
-        if (newValue.text.length > mask.length) return oldValue;
-        if (newValue.text.length < mask.length &&
-            mask[newValue.text.length - 1] == separator) {
-          return TextEditingValue(
-            text: '${oldValue.text}$separator'
-                '${newValue.text.substring(newValue.text.length - 1)}',
-            selection: TextSelection.collapsed(
-              offset: newValue.selection.end + 1,
-            ),
-          );
-        }
+    final difference = newValue.text.length - oldValue.text.length;
+
+    if (newValue.text.length > mask.length) return oldValue;
+
+    // paste text value or start typing
+    if (oldValue.text.isEmpty) {
+      final newValue2 = newValue.text.split('');
+      final mask2 = mask.split('');
+      newValue2.removeWhere((element) => element == separator);
+      mask2.removeWhere((element) => element == separator);
+
+      // start typing
+      if (difference == 1) {
+        return _value(mask, separator, newValue.text);
+      }
+
+      // paste text value
+      if (newValue2.length == mask2.length) {
+        return _value(mask, separator, newValue.text);
       }
     }
-    return newValue;
+    // typing
+    else if (difference == 1) {
+      return _value(mask, separator, newValue.text);
+    }
+    // backspace or cut
+    else if (difference < 1) {
+      return newValue;
+    }
+
+    // cases when user pastes text and oldValue is not empty
+    return oldValue;
+  }
+
+  TextEditingValue _value(
+    String mask,
+    String seperator,
+    String newValue,
+  ) {
+    final newValue2 = newValue.split('');
+    newValue2.removeWhere((element) => element == seperator);
+    var text = '';
+    var index = 0;
+
+    for (final char in mask.split('')) {
+      if (char == seperator) {
+        text += seperator;
+      } else {
+        if (index == newValue2.length) break;
+        text += newValue2[index];
+        index++;
+      }
+    }
+
+    return TextEditingValue(
+      text: text,
+      selection: TextSelection.collapsed(
+        offset: text.length,
+      ),
+    );
   }
 }
