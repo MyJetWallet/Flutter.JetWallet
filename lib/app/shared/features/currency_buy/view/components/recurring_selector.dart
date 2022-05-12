8f899243c1c9603fbb7d35d3db663c22574f3520
Eli Stark <elistark.tech@gmail.com>
2022-05-12 07:14:05 +0300

    SPUI-1601 > Hide recurring selector on buy when simplex is active (#685)
    * SPUI-1601 > Hide recurring selector on buy when simplex is active
    
    * fix bug
    
    * update ci

--- lib/app/shared/features/currency_buy/view/components/recurring_selector.dart
+++ lib/app/shared/features/currency_buy/view/components/recurring_selector.dart
@@ -3,73 +3,76 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:simple_kit/simple_kit.dart';
 
-import '../../../../models/currency_model.dart';
 import '../../../actions/action_recurring_buy/action_with_out_recurring_buy.dart';
 import '../../../recurring/helper/recurring_buys_operation_name.dart';
-import '../../notifier/currency_buy_notifier/currency_buy_notipod.dart';
 
 class RecurringSelector extends HookWidget {
   const RecurringSelector({
     Key? key,
-    required this.currency,
+    this.oneTimePurchaseOnly = false,
+    required this.currentSelection,
+    required this.onSelect,
   }) : super(key: key);
 
-  final CurrencyModel currency;
+  final bool oneTimePurchaseOnly;
+  final RecurringBuysType currentSelection;
+  final void Function(RecurringBuysType) onSelect;
 
   @override
   Widget build(BuildContext context) {
     final colors = useProvider(sColorPod);
-    final state = useProvider(currencyBuyNotipod(currency));
-    final notifier = useProvider(currencyBuyNotipod(currency).notifier);
 
     return Row(
       children: [
         const Spacer(),
-        GestureDetector(
-          onTap: () {
-            showActionWithOutRecurringBuy(
-              context: context,
-              title: 'Repeat this purchase?',
-              showOneTimePurchase: true,
-              currentType: state.recurringBuyType,
-              onItemTap: (recurringType) {
-                notifier.updateRecurringBuyType(recurringType);
-                Navigator.of(context).pop();
-              },
-            );
-          },
-          child: Container(
-            height: 28,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 2,
+        if (oneTimePurchaseOnly)
+          Text(
+            'One-time purchase only',
+            style: sSubtitle3Style.copyWith(
+              color: colors.grey1,
             ),
-            decoration: BoxDecoration(
-              color: colors.blue,
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Row(
-              children: [
-                Baseline(
-                  baseline: 14,
-                  baselineType: TextBaseline.alphabetic,
-                  child: Text(
-                    recurringBuysOperationName(
-                      state.recurringBuyType,
-                    ),
-                    style: sSubtitle3Style.copyWith(
-                      color: colors.white,
+          )
+        else
+          GestureDetector(
+            onTap: () {
+              showActionWithOutRecurringBuy(
+                context: context,
+                title: 'Repeat this purchase?',
+                showOneTimePurchase: true,
+                currentType: currentSelection,
+                onItemTap: onSelect,
+              );
+            },
+            child: Container(
+              height: 28,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 2,
+              ),
+              decoration: BoxDecoration(
+                color: colors.blue,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Row(
+                children: [
+                  Baseline(
+                    baseline: 14,
+                    baselineType: TextBaseline.alphabetic,
+                    child: Text(
+                      recurringBuysOperationName(currentSelection),
+                      style: sSubtitle3Style.copyWith(
+                        color: colors.white,
+                      ),
                     ),
                   ),
-                ),
-                const SpaceW8(),
-                SAngleDownIcon(
-                  color: colors.white,
-                ),
-              ],
+                  const SpaceW8(),
+                  SAngleDownIcon(
+                    color: colors.white,
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
         const Spacer(),
       ],
     );
