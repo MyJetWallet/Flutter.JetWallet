d2a65234594e6eac0260df633971175093adde77
Eli Stark <elistark.tech@gmail.com>
2022-05-17 00:13:00 -0700

    update

--- lib/service/shared/helpers/handle_api_responses.dart
+++ lib/service/shared/helpers/handle_api_responses.dart
@@ -1,3 +1,4 @@
+import '../../../shared/helpers/timespan_to_duration.dart';
 import '../models/server_reject_exception.dart';
 import 'error_codes_description.dart';
 
@@ -30,7 +31,7 @@ void _validateFullResponse(String result, Map<String, dynamic> json) {
     final data = json['data'] as Map<String, dynamic>;
     final expire = data['blockerExpired'] as String;
 
-    throw ServerRejectException(_blockerMessage(expire));
+    throw ServerRejectException(_blockerMessage(timespanToDuration(expire)));
   } else if (result != 'OK') {
     throw ServerRejectException(errorCodesDescription[result] ?? result);
   }
@@ -42,46 +43,32 @@ void _validateResultResponse(String result) {
   }
 }
 
-String _blockerMessage(String expire) {
+String _blockerMessage(Duration duration) {
   const phrase1 = 'Due to several failed log in attempts access '
       'to this account will be suspended';
   const phrase2 = ' for';
 
-  final split = expire.split(':');
-  var hours = split[0];
-  final minutes = split[1];
-  var seconds = split[2];
+  final d = duration.inDays;
+  final h = duration.inHours;
+  final m = duration.inMinutes;
+  final s = duration.inSeconds;
 
-  if (hours[0] == '-') {
-    hours = hours.substring(1);
-  }
+  final dEnd = _pluralEnd(d);
+  final hEnd = _pluralEnd(h);
+  final mEnd = _pluralEnd(m);
+  final sEnd = _pluralEnd(s);
 
-  if (hours.contains('.')) {
-    return '$phrase1.';
-  }
-
-  seconds = seconds.substring(0, 2);
-
-  var hInt = int.parse(hours);
-  final dInt = hInt ~/ 24;
-  hInt = hInt % 24;
-  final mInt = int.parse(minutes);
-  final sInt = int.parse(seconds);
-
-  final dEnd = dInt == 1 ? '' : 's';
-  final hEnd = hInt == 1 ? '' : 's';
-  final mEnd = mInt == 1 ? '' : 's';
-  final sEnd = sInt == 1 ? '' : 's';
-
-  if (dInt != 0) {
-    return '$phrase1$phrase2 $dInt day$dEnd $hInt hour$hEnd.';
-  } else if (hInt != 0) {
-    return '$phrase1$phrase2 $hInt hour$hEnd $mInt minute$mEnd.';
-  } else if (mInt != 0) {
-    return '$phrase1$phrase2 $mInt minute$mEnd.';
-  } else if (sInt != 0) {
-    return '$phrase1$phrase2 $sInt second$sEnd.';
+  if (d != 0) {
+    return '$phrase1$phrase2 $d day$dEnd $h hour$hEnd.';
+  } else if (h != 0) {
+    return '$phrase1$phrase2 $h hour$hEnd $m minute$mEnd.';
+  } else if (m != 0) {
+    return '$phrase1$phrase2 $m minute$mEnd.';
+  } else if (s != 0) {
+    return '$phrase1$phrase2 $s second$sEnd.';
   } else {
     return '$phrase1.';
   }
 }
+
+String _pluralEnd(int number) => number == 1 ? '' : 's';
