181b9b91aa4a6c924cabfbd59af3c044d2c9fa3e
Eli Stark <elistark.tech@gmail.com>
2022-05-25 06:41:50 +0300

    SPUI-1363 > Add alert popup on change password and phone (#705)
    * SPUI-1363 > Add alert popup on change password and phone
    
    * update remote config
    
    * update with localization
    
    * fix

--- lib/shared/services/remote_config_service/remote_config_values.dart
+++ lib/shared/services/remote_config_service/remote_config_values.dart
@@ -22,6 +22,8 @@ late int emailResendCountdown;
 late int withdrawalConfirmResendCountdown;
 late int localPinLength;
 late int maxPinAttempts;
+late int changePasswordLockHours;
+late int changePhoneLockHours;
 
 // Versioning
 late String recommendedVersion;
