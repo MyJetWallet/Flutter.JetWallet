e0a7b5269a582027dda1de712e2ae7e2e37ed13a
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2022-05-25 13:04:21 +0300

    SPUI-1429 > Fix building error

--- lib/app/screens/portfolio/view/components/empty_portfolio/components/earn_bottom_sheet/components/earn_bottom_sheet_container.dart
+++ lib/app/screens/portfolio/view/components/empty_portfolio/components/earn_bottom_sheet/components/earn_bottom_sheet_container.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:simple_kit/simple_kit.dart';
 
-import '../../../../../../../../../shared/providers/device_size/media_query_pod.dart';
 import '../../../../../../../market/view/components/change_on_scroll.dart';
 
 class EarnBottomSheetContainer extends StatefulHookWidget {
