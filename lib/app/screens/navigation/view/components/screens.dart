f59cc9e1d3b11a7b8b081759cd33c18f24896f62
Dmitry <graviohz@gmail.com>
2022-05-27 01:36:44 +0300

    Disable earn

--- lib/app/screens/navigation/view/components/screens.dart
+++ lib/app/screens/navigation/view/components/screens.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 import '../../../account/account.dart';
-import '../../../earn/earn.dart';
 import '../../../market/view/market.dart';
+//import '../../../earn/earn.dart';
+import '../../../news/view/news.dart';
 import '../../../portfolio/view/portfolio.dart';
 
 List<Widget> screens = [
   const Market(),
   const Portfolio(),
-  const Earn(),
+  //const Earn(),
+  const News(),
   const Account(),
 ];
