cfd4795ed8316b6e4c1b596ac65e979f701e770b
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2022-08-10 12:09:05 +0300

    SPUI-2098 > Time checker

--- lib/core/stage/components/remote_config_init.dart
+++ lib/core/stage/components/remote_config_init.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:simple_analytics/simple_analytics.dart';
 
 import '../../../auth/screens/splash/splash_screen.dart';
 import '../../../router/notifier/remote_config_notifier/remote_config_notipod.dart';
-
-import '../../../shared/notifiers/time_tracking_notifier/time_tracking_notipod.dart';
 
 /// Fetches and activates remote config, needed to go first after [AppRouter]
 class RemoteConfigInit extends HookWidget {
@@ -20,14 +17,9 @@ class RemoteConfigInit extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final remoteConfig = useProvider(remoteConfigNotipod);
-    final timeTrackerN = useProvider(timeTrackingNotipod.notifier);
 
     return remoteConfig.when(
       success: () {
-        WidgetsBinding.instance.addPostFrameCallback((_) async {
-          sAnalytics.remoteConfig();
-          await timeTrackerN.updateConfigReceived(DateTime.now());
-        });
         return child;
       },
       loading: () => const SplashScreen(),
