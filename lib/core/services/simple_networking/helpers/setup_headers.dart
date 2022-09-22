93ddaed43269538474d6c02bfaeeb030058f0c64
Dmitry <graviohz@gmail.com>
2022-09-23 00:38:40 +0300

    fix headers

--- lib/core/services/simple_networking/helpers/setup_headers.dart
+++ lib/core/services/simple_networking/helpers/setup_headers.dart
@@ -18,8 +18,6 @@ void setupHeaders(Dio dio, [String? token]) {
   final deviceSize = mediaQuery.size;
   final devicePixelRatio = mediaQuery.devicePixelRatio;
 
-  final lang = intl.localeName;
-
   dio.options.headers['accept'] = 'application/json';
   dio.options.headers['content-Type'] = 'application/json';
   dio.options.headers['Authorization'] = 'Bearer $token';
@@ -27,5 +25,5 @@ void setupHeaders(Dio dio, [String? token]) {
   dio.options.headers['From'] = deviceInfo.deviceUid;
   dio.options.headers['User-Agent'] =
       '$appVersion;${packageInfo.buildNumber};$deviceType;$deviceSize;'
-      '$devicePixelRatio;${deviceInfo.marketingName};$lang';
+      '$devicePixelRatio;${deviceInfo.marketingName};$locale';
 }
