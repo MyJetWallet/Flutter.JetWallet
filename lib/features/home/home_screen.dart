00c045fe0cfb864a185ee837ea847c5b805c9cb8
Dmitry <graviohz@gmail.com>
2022-11-03 20:28:52 +0200

    fix action menu black

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -58,26 +58,30 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
-    return AutoTabsScaffold(
-      routes: earnEnabled ? screens : screensWithNews,
-      builder: (context, child, animation) {
-        return SShadeAnimationStack(
-          showShade: getIt.get<AppStore>().actionMenuActive,
-          child: FadeTransition(
-            opacity: animation,
-            child: child,
-          ),
-        );
-      },
-      bottomNavigationBuilder: (_, tabsRouter) {
-        getIt.get<AppStore>().setTabsRouter(tabsRouter);
+    return Observer(
+      builder: (context) {
+        return AutoTabsScaffold(
+          routes: earnEnabled ? screens : screensWithNews,
+          builder: (context, child, animation) {
+            return SShadeAnimationStack(
+              showShade: getIt.get<AppStore>().actionMenuActive,
+              child: FadeTransition(
+                opacity: animation,
+                child: child,
+              ),
+            );
+          },
+          bottomNavigationBuilder: (_, tabsRouter) {
+            getIt.get<AppStore>().setTabsRouter(tabsRouter);
 
-        return BottomNavigationMenu(
-          transitionAnimationController: animationController,
-          currentIndex: getIt.get<AppStore>().homeTab,
-          onChanged: (int val) {
-            getIt<AppStore>().setHomeTab(val);
-            tabsRouter.setActiveIndex(val);
+            return BottomNavigationMenu(
+              transitionAnimationController: animationController,
+              currentIndex: getIt.get<AppStore>().homeTab,
+              onChanged: (int val) {
+                getIt<AppStore>().setHomeTab(val);
+                tabsRouter.setActiveIndex(val);
+              },
+            );
           },
         );
       },
