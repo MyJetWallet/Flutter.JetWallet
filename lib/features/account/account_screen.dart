882a2bbe5b5c1f07613444c60670e4f44cd22c58
Dmitry <graviohz@gmail.com>
2022-12-27 12:26:26 +0200

    Fix log button

--- lib/features/account/account_screen.dart
+++ lib/features/account/account_screen.dart
@@ -49,6 +49,7 @@ class _AccountScreenState extends State<AccountScreen>
     final kycState = getIt.get<KycService>();
     final kycAlertHandler = getIt.get<KycAlertHandler>();
 
+    /*
     logout.union.when(
       result: (error, st) {
         if (error != null) {
@@ -57,220 +58,216 @@ class _AccountScreenState extends State<AccountScreen>
       },
       loading: () {},
     );
+    */
 
-    return logout.union.when(
-      result: (_, __) {
-        return Material(
-          color: colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              SPaddingH24(
-                child: SimpleAccountCategoryHeader(
-                  onIconTap: () {
-                    if (debugTapCounter >= 4) {
-                      sRouter.push(
-                        const DebugInfoRouter(),
-                      );
-                      setState(() {
-                        debugTapCounter = 0;
-                      });
-                    } else {
-                      setState(() {
-                        debugTapCounter++;
-                      });
-                    }
+    return Material(
+      color: colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SPaddingH24(
+            child: SimpleAccountCategoryHeader(
+              onIconTap: () {
+                if (debugTapCounter >= 4) {
+                  sRouter.push(
+                    const DebugInfoRouter(),
+                  );
+                  setState(() {
+                    debugTapCounter = 0;
+                  });
+                } else {
+                  setState(() {
+                    debugTapCounter++;
+                  });
+                }
+              },
+              userEmail: authInfo.email,
+              userFirstName: userInfo.firstName,
+              userLastName: userInfo.lastName,
+              showUserName:
+                  userInfo.firstName.isNotEmpty && userInfo.lastName.isNotEmpty,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                AccountBannerList(
+                  kycPassed: checkKycPassed(
+                    kycState.depositStatus,
+                    kycState.sellStatus,
+                    kycState.withdrawalStatus,
+                  ),
+                  kycBlocked: checkKycBlocked(
+                    kycState.depositStatus,
+                    kycState.sellStatus,
+                    kycState.withdrawalStatus,
+                  ),
+                  verificationInProgress: kycState.inVerificationProgress,
+                  twoFaEnabled: true,
+                  phoneVerified: userInfo.phoneVerified,
+                  onTwoFaBannerTap: () {
+                    sAnalytics.bannerClick(
+                      bannerName: '2-Factor Authentication',
+                    );
+
+                    sRouter.push(const SmsAuthenticatorRouter());
                   },
-                  userEmail: authInfo.email,
-                  userFirstName: userInfo.firstName,
-                  userLastName: userInfo.lastName,
-                  showUserName: userInfo.firstName.isNotEmpty &&
-                      userInfo.lastName.isNotEmpty,
-                ),
-              ),
-              Expanded(
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    AccountBannerList(
-                      kycPassed: checkKycPassed(
-                        kycState.depositStatus,
-                        kycState.sellStatus,
-                        kycState.withdrawalStatus,
-                      ),
-                      kycBlocked: checkKycBlocked(
-                        kycState.depositStatus,
-                        kycState.sellStatus,
-                        kycState.withdrawalStatus,
-                      ),
-                      verificationInProgress: kycState.inVerificationProgress,
-                      twoFaEnabled: true,
-                      phoneVerified: userInfo.phoneVerified,
-                      onTwoFaBannerTap: () {
-                        sAnalytics.bannerClick(
-                          bannerName: '2-Factor Authentication',
-                        );
+                  onChatBannerTap: () {
+                    sAnalytics.bannerClick(
+                      bannerName: 'Chat with support',
+                    );
 
-                        sRouter.push(const SmsAuthenticatorRouter());
-                      },
-                      onChatBannerTap: () {
-                        sAnalytics.bannerClick(
-                          bannerName: 'Chat with support',
-                        );
+                    sRouter.push(
+                      CrispRouter(
+                        welcomeText: intl.crispSendMessage_hi,
+                      ),
+                    );
+                  },
+                  onKycBannerTap: () {
+                    sAnalytics.bannerClick(
+                      bannerName: 'KYC banner',
+                    );
+                    defineKycVerificationsScope(
+                      kycState.requiredVerifications.length,
+                      Source.accountBanner,
+                    );
+                    final isDepositAllow = kycState.depositStatus !=
+                        kycOperationStatus(KycStatus.allowed);
+                    final isWithdrawalAllow = kycState.withdrawalStatus !=
+                        kycOperationStatus(KycStatus.allowed);
+
+                    kycAlertHandler.handle(
+                      status: isDepositAllow
+                          ? kycState.depositStatus
+                          : isWithdrawalAllow
+                              ? kycState.withdrawalStatus
+                              : kycState.sellStatus,
+                      isProgress: kycState.verificationInProgress,
+                      currentNavigate: () {},
+                      requiredDocuments: kycState.requiredDocuments,
+                      requiredVerifications: kycState.requiredVerifications,
+                    );
+                  },
+                ),
+                Column(
+                  children: <Widget>[
+                    SimpleAccountCategoryButton(
+                      title: intl.account_profileDetails,
+                      icon: const SProfileDetailsIcon(),
+                      isSDivider: true,
+                      onTap: () {
+                        sAnalytics.account();
 
                         sRouter.push(
-                          CrispRouter(
-                            welcomeText: intl.crispSendMessage_hi,
-                          ),
-                        );
-                      },
-                      onKycBannerTap: () {
-                        sAnalytics.bannerClick(
-                          bannerName: 'KYC banner',
-                        );
-                        defineKycVerificationsScope(
-                          kycState.requiredVerifications.length,
-                          Source.accountBanner,
-                        );
-                        final isDepositAllow = kycState.depositStatus !=
-                            kycOperationStatus(KycStatus.allowed);
-                        final isWithdrawalAllow = kycState.withdrawalStatus !=
-                            kycOperationStatus(KycStatus.allowed);
-
-                        kycAlertHandler.handle(
-                          status: isDepositAllow
-                              ? kycState.depositStatus
-                              : isWithdrawalAllow
-                                  ? kycState.withdrawalStatus
-                                  : kycState.sellStatus,
-                          isProgress: kycState.verificationInProgress,
-                          currentNavigate: () {},
-                          requiredDocuments: kycState.requiredDocuments,
-                          requiredVerifications: kycState.requiredVerifications,
+                          const ProfileDetailsRouter(),
                         );
                       },
                     ),
-                    Column(
-                      children: <Widget>[
-                        SimpleAccountCategoryButton(
-                          title: intl.account_profileDetails,
-                          icon: const SProfileDetailsIcon(),
-                          isSDivider: true,
-                          onTap: () {
-                            sAnalytics.account();
-
-                            sRouter.push(
-                              const ProfileDetailsRouter(),
-                            );
-                          },
-                        ),
-                        SimpleAccountCategoryButton(
-                          title: intl.account_security,
-                          icon: const SSecurityIcon(),
-                          isSDivider: true,
-                          onTap: () {
-                            sRouter.push(
-                              const AccountSecurityRouter(),
-                            );
-                          },
-                        ),
-                        SimpleAccountCategoryButton(
-                          title: intl.account_paymentMethods,
-                          icon: SActionDepositIcon(
-                            color: colors.black,
-                          ),
-                          isSDivider: true,
-                          notification: false,
-                          onTap: () {
-                            sAnalytics.paymentTap();
-                            sAnalytics.paymentView();
-                            sRouter.push(
-                              const PaymentMethodsRouter(),
-                            );
-                          },
-                        ),
-                        SimpleAccountCategoryButton(
-                          title: intl.account_recurringBuy,
-                          icon: const SRecurringBuysIcon(),
-                          isSDivider: true,
-                          onTap: () {
-                            sRouter.push(
-                              HistoryRecurringBuysRouter(
-                                from: Source.profile,
-                              ),
-                            );
-                          },
-                        ),
-                        SimpleAccountCategoryButton(
-                          title: intl.account_history,
-                          icon: const SIndexHistoryIcon(),
-                          isSDivider: true,
-                          onTap: () => sRouter.push(
-                            TransactionHistoryRouter(),
-                          ),
-                        ),
-                        SimpleAccountCategoryButton(
-                          title: intl.account_support,
-                          icon: const SSupportIcon(),
-                          isSDivider: true,
-                          onTap: () => sRouter.push(
-                            CrispRouter(
-                              welcomeText: intl.crispSendMessage_hi,
-                            ),
-                          ),
-                        ),
-                        SimpleAccountCategoryButton(
-                          title: intl.account_helpCenter,
-                          icon: const SQuestionIcon(),
-                          isSDivider: true,
-                          onTap: () {
-                            sRouter.push(
-                              HelpCenterWebViewRouter(
-                                link: faqLink,
-                              ),
-                            );
-                          },
-                        ),
-                        SimpleAccountCategoryButton(
-                          title: intl.account_aboutUs,
-                          icon: const SAboutUsIcon(),
-                          isSDivider: flavor == Flavor.dev,
-                          onTap: () {
-                            sRouter.push(
-                              const AboutUsRouter(),
-                            );
-                          },
-                        ),
-                        if (flavor == Flavor.dev || flavor == Flavor.stage)
-                          SimpleAccountCategoryButton(
-                            title: intl.account_debugInfo,
-                            icon: const SInfoIcon(),
-                            isSDivider: false,
-                            onTap: () {
-                              sRouter.push(
-                                const DebugInfoRouter(),
-                              );
-                            },
-                          ),
-                      ],
+                    SimpleAccountCategoryButton(
+                      title: intl.account_security,
+                      icon: const SSecurityIcon(),
+                      isSDivider: true,
+                      onTap: () {
+                        sRouter.push(
+                          const AccountSecurityRouter(),
+                        );
+                      },
                     ),
-                    const SpaceH20(),
-                    const SDivider(),
-                    const SpaceH20(),
-                    LogOutOption(
-                      name: intl.log_out,
-                      onTap: () => logout.logout('account logout'),
+                    SimpleAccountCategoryButton(
+                      title: intl.account_paymentMethods,
+                      icon: SActionDepositIcon(
+                        color: colors.black,
+                      ),
+                      isSDivider: true,
+                      notification: false,
+                      onTap: () {
+                        sAnalytics.paymentTap();
+                        sAnalytics.paymentView();
+                        sRouter.push(
+                          const PaymentMethodsRouter(),
+                        );
+                      },
                     ),
-                    const SpaceH20(),
+                    SimpleAccountCategoryButton(
+                      title: intl.account_recurringBuy,
+                      icon: const SRecurringBuysIcon(),
+                      isSDivider: true,
+                      onTap: () {
+                        sRouter.push(
+                          HistoryRecurringBuysRouter(
+                            from: Source.profile,
+                          ),
+                        );
+                      },
+                    ),
+                    SimpleAccountCategoryButton(
+                      title: intl.account_history,
+                      icon: const SIndexHistoryIcon(),
+                      isSDivider: true,
+                      onTap: () => sRouter.push(
+                        TransactionHistoryRouter(),
+                      ),
+                    ),
+                    SimpleAccountCategoryButton(
+                      title: intl.account_support,
+                      icon: const SSupportIcon(),
+                      isSDivider: true,
+                      onTap: () => sRouter.push(
+                        CrispRouter(
+                          welcomeText: intl.crispSendMessage_hi,
+                        ),
+                      ),
+                    ),
+                    SimpleAccountCategoryButton(
+                      title: intl.account_helpCenter,
+                      icon: const SQuestionIcon(),
+                      isSDivider: true,
+                      onTap: () {
+                        sRouter.push(
+                          HelpCenterWebViewRouter(
+                            link: faqLink,
+                          ),
+                        );
+                      },
+                    ),
+                    SimpleAccountCategoryButton(
+                      title: intl.account_aboutUs,
+                      icon: const SAboutUsIcon(),
+                      isSDivider: flavor == Flavor.dev,
+                      onTap: () {
+                        sRouter.push(
+                          const AboutUsRouter(),
+                        );
+                      },
+                    ),
+                    if (flavor == Flavor.dev || flavor == Flavor.stage)
+                      SimpleAccountCategoryButton(
+                        title: intl.account_debugInfo,
+                        icon: const SInfoIcon(),
+                        isSDivider: false,
+                        onTap: () {
+                          sRouter.push(
+                            const DebugInfoRouter(),
+                          );
+                        },
+                      ),
                   ],
                 ),
-              ),
-            ],
+                const SpaceH20(),
+                const SDivider(),
+                const SpaceH20(),
+                LogOutOption(
+                  name: intl.log_out,
+                  onTap: () => logout.logout('account logout'),
+                ),
+                const SpaceH20(),
+              ],
+            ),
           ),
-        );
-      },
-      loading: () => const Loader(),
+        ],
+      ),
     );
   }
 }
