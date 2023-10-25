50a00f48eb8dce15093f41a82970b66ed57bfc82
Dmitry <graviohz@gmail.com>
2023-10-25 23:21:08 +0300

    update

--- lib/features/app/app.dart
+++ lib/features/app/app.dart
@@ -10,6 +10,7 @@ import 'package:jetwallet/features/app/app_builder.dart';
 import 'package:jetwallet/features/app/route_observer.dart';
 import 'package:jetwallet/utils/logging.dart';
 import 'package:logging/logging.dart';
+import 'package:sift/sift.dart';
 
 class AppScreen extends StatefulWidget {
   const AppScreen({
@@ -50,6 +51,8 @@ class _AppScreenState extends State<AppScreen> {
       getIt.registerSingleton<PushNotification>(
         PushNotification(),
       );
+
+      getPlatform();
     } catch (e) {
       _logger.log(
         notifier,
@@ -58,6 +61,16 @@ class _AppScreenState extends State<AppScreen> {
     }
 
     super.initState();
+  }
+
+  Future<void> getPlatform() async {
+    final _siftPlugin = Sift();
+
+    print('INIT SIFT');
+
+    var platformVersion = await _siftPlugin.getPlatformVersion() ?? 'Unknown platform version';
+
+    print(platformVersion);
   }
 
   @override
