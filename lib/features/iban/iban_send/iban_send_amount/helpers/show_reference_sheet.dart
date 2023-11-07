114773bd31ee54d405442778f4fad407099e1e8b
Dmitry <graviohz@gmail.com>
2023-11-07 13:10:18 +0200

    SPU-3714

--- lib/features/iban/iban_send/iban_send_amount/helpers/show_reference_sheet.dart
+++ lib/features/iban/iban_send/iban_send_amount/helpers/show_reference_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:jetwallet/core/l10n/i10n.dart';
 import 'package:jetwallet/core/router/app_router.dart';
 import 'package:simple_kit/simple_kit.dart';
@@ -13,6 +14,11 @@ abstract class __ReferenceStoreBase with Store {
   final referenceTextField = TextEditingController(
     text: 'Sent from Simple',
   );
+
+  @observable
+  bool isError = false;
+  @action
+  void setError(bool value) => isError = value;
 
   @action
   Future<void> paste() async {
@@ -42,54 +48,92 @@ void showReferenceSheet(BuildContext context, Function(String) onContinue) {
   sShowBasicModalBottomSheet(
     context: context,
     children: [
-      SPaddingH24(
-        child: Text(
-          intl.iban_reference,
-          style: sTextH4Style,
-        ),
-      ),
-      const SpaceH20(),
-      SPaddingH24(
-        child: SStandardField(
-          controller: store.referenceTextField,
-          autofocus: true,
-          labelText: intl.iban_reference,
-          suffixIcons: [
-            SIconButton(
-              onTap: () {
-                store.paste();
-              },
-              defaultIcon: const SPasteIcon(),
-              pressedIcon: const SPastePressedIcon(),
-            ),
-          ],
-          onErase: () {},
-          onChanged: (value) {},
-        ),
-      ),
-      Material(
-        color: sKit.colors.grey5,
-        child: SPaddingH24(
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              children: [
-                const SpaceH32(),
-                SPrimaryButton2(
-                  active: true,
-                  name: intl.withdraw_continue,
-                  onTap: () {
-                    onContinue(store.referenceTextField.text);
+      _ReferenceBody(
+        store: store,
+        onContinue: onContinue,
+      )
+    ],
+  );
+}
 
-                    sRouter.pop();
-                  },
-                ),
-                const SpaceH24(),
-              ],
+class _ReferenceBody extends StatelessObserverWidget {
+  const _ReferenceBody({
+    Key? key,
+    required this.store,
+    required this.onContinue,
+  }) : super(key: key);
+
+  final _ReferenceStore store;
+  final Function(String) onContinue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SPaddingH24(
+          child: Text(
+            intl.iban_reference,
+            style: sTextH4Style,
+          ),
+        ),
+        const SpaceH20(),
+        Observer(
+          builder: (context) {
+            return SPaddingH24(
+              child: SStandardField(
+                controller: store.referenceTextField,
+                autofocus: true,
+                isError: store.isError,
+                labelText: intl.iban_reference,
+                suffixIcons: [
+                  SIconButton(
+                    onTap: () {
+                      store.paste();
+                    },
+                    defaultIcon: const SPasteIcon(),
+                    pressedIcon: const SPastePressedIcon(),
+                  ),
+                ],
+                onErase: () {},
+                onChanged: (value) {
+                  store.setError(false);
+                },
+              ),
+            );
+          },
+        ),
+        Material(
+          color: sKit.colors.grey5,
+          child: SPaddingH24(
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SpaceH32(),
+                  SPrimaryButton2(
+                    active: true,
+                    name: intl.withdraw_continue,
+                    onTap: () {
+                      if (store.referenceTextField.text.length > 5) {
+                        onContinue(store.referenceTextField.text);
+
+                        sRouter.pop();
+                      } else {
+                        store.setError(true);
+
+                        print(store.isError);
+                      }
+
+                      return;
+                    },
+                  ),
+                  const SpaceH24(),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
