79938ea557284e3a5ef9bb2ae5eae26bb10a3b13
Dmitry <graviohz@gmail.com>
2023-11-09 20:25:01 +0200

    SPU-3649

--- lib/features/wallet/ui/widgets/wallet_header.dart
+++ lib/features/wallet/ui/widgets/wallet_header.dart
@@ -93,16 +93,13 @@ class WalletHeader extends StatelessWidget {
                           overflow: TextOverflow.ellipsis,
                         ),
                       if (baseCurrency.symbol != curr.symbol)
-                        Opacity(
-                          opacity: curr.symbol == 'EUR' ? 0 : 1,
-                          child: Text(
-                            curr.volumeAssetBalance,
-                            style: sBodyText2Style.copyWith(
-                              color: sKit.colors.grey1,
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+                        Text(
+                          isEurWallet ? curr.volumeBaseBalance(baseCurrency) : curr.volumeAssetBalance,
+                          style: sBodyText2Style.copyWith(
+                            color: sKit.colors.grey1,
                           ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                         ),
                       const SpaceH16(),
                       if (pageCount > 1)
