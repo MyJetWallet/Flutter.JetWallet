979dd56f6ec8d301af50e1a05d53a7553852d427
Dmitry <graviohz@gmail.com>
2023-11-17 13:23:05 +0200

    ZenDesk

--- plugins/simple_networking/lib/modules/remote_config/models/remote_config_support_model.dart
+++ plugins/simple_networking/lib/modules/remote_config/models/remote_config_support_model.dart
@@ -6,10 +6,12 @@ part 'remote_config_support_model.g.dart';
 @freezed
 class RemoteConfigSupportModel with _$RemoteConfigSupportModel {
   factory RemoteConfigSupportModel({
-    required String faqLink,
-    required String crispWebsiteId,
+    String? faqLink,
+    String? crispWebsiteId,
+    bool? showZendesk,
+    String? zendeskIOS,
+    String? zendeskAndroid,
   }) = _RemoteConfigSupportModel;
 
-  factory RemoteConfigSupportModel.fromJson(Map<String, dynamic> json) =>
-      _$RemoteConfigSupportModelFromJson(json);
+  factory RemoteConfigSupportModel.fromJson(Map<String, dynamic> json) => _$RemoteConfigSupportModelFromJson(json);
 }
