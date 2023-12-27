a05fc3b1b6fd90192519a707b44f8681916594d2
Dmitry <graviohz@gmail.com>
2023-12-27 16:01:09 +0200

    SPU-3905 > Order summary (#1353)
    * send order summary
    
    * SPU-3905
    
    * SPU-3905
    
    * update locale

--- plugins/simple_kit_updated/lib/widgets/table/cells/two_column_cell.dart
+++ plugins/simple_kit_updated/lib/widgets/table/cells/two_column_cell.dart
@@ -8,42 +8,49 @@ class TwoColumnCell extends StatelessWidget {
     Key? key,
     required this.label,
     this.value,
+    this.rightValueIcon,
     this.needHorizontalPadding = true,
   }) : super(key: key);
 
   final String label;
   final String? value;
+  final Widget? rightValueIcon;
 
   final bool needHorizontalPadding;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 40,
+      //height: 40,
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: needHorizontalPadding ? 24 : 0, vertical: 8),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                Text(
-                  label,
-                  style: STStyles.body2Medium.copyWith(color: SColorsLight().gray10),
-                ),
-                const Gap(8),
-              ],
-            ),
-            const Spacer(),
-            Row(
-              children: [
-                if (value != null)
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * .3,
+              ),
+              child: Row(
+                children: [
                   Text(
-                    value ?? '',
-                    textAlign: TextAlign.right,
-                    style: STStyles.subtitle2,
+                    label,
+                    style: STStyles.body2Medium.copyWith(color: SColorsLight().gray10),
                   ),
-              ],
+                  const Gap(8),
+                ],
+              ),
             ),
+            const Gap(10),
+            if (value != null)
+              Expanded(
+                child: Text(
+                  value ?? '',
+                  maxLines: 2,
+                  textAlign: TextAlign.right,
+                  style: STStyles.subtitle2,
+                ),
+              ),
           ],
         ),
       ),
