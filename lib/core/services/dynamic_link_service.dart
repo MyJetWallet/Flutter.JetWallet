06dc62ea193590975250fde216e1394165cfbfcf
Dmitry <graviohz@gmail.com>
2024-01-03 12:26:34 +0200

    android integration

--- lib/core/services/dynamic_link_service.dart
+++ lib/core/services/dynamic_link_service.dart
@@ -1,14 +1,52 @@
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+//import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'dart:async';
+
 import 'package:jetwallet/core/di/di.dart';
 import 'package:jetwallet/core/services/deep_link_service.dart';
 import 'package:jetwallet/core/services/logger_service/logger_service.dart';
 import 'package:logger/logger.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:flutter/services.dart' show PlatformException;
 
 class DynamicLinkService {
   //getIt.get<DeepLinkService>().handle
-  final _firebaseDynamicLinks = FirebaseDynamicLinks.instance;
+  //final _firebaseDynamicLinks = FirebaseDynamicLinks.instance;
 
   Future<void> initDynamicLinks() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      final initialLink = await getInitialUri();
+
+      getIt.get<SimpleLoggerService>().log(
+        level: Level.warning,
+        place: 'DynamicLinkService',
+        message: '_handleDynamicLink: $initialLink',
+      );
+
+
+      getIt.get<DeepLinkService>().handle(initialLink!, fromBG: true);
+    } on PlatformException {
+      // Handle exception by warning the user their action did not succeed
+      // return?
+    }
+
+    StreamSubscription _sub;
+
+    _sub = uriLinkStream.listen((Uri? uri) {
+      getIt.get<SimpleLoggerService>().log(
+        level: Level.warning,
+        place: 'DynamicLinkService',
+        message: '_handleDynamicLink: $uri',
+      );
+
+      getIt.get<DeepLinkService>().handle(uri!, fromBG: true);
+
+    }, onError: (err) {
+      // Handle exception by warning the user their action did not succeed
+    });
+
+
+    /*
     // Get the initial dynamic link if the app is opened with a dynamic link
     final data = await _firebaseDynamicLinks.getInitialLink();
 
@@ -39,8 +77,10 @@ class DynamicLinkService {
             );
       },
     );
+    */
   }
 
+  /*
   void _handleDynamicLink(
     PendingDynamicLinkData? data,
     void Function(Uri) handler,
@@ -57,4 +97,5 @@ class DynamicLinkService {
       handler(link);
     }
   }
+  */
 }
