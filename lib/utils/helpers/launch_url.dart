04bd8665f77658d5c115586bd3bd170087ec6434
Dmitry <graviohz@gmail.com>
2024-01-09 18:35:54 +0200

    SPU-4103

--- lib/utils/helpers/launch_url.dart
+++ lib/utils/helpers/launch_url.dart
@@ -3,15 +3,15 @@ import 'package:jetwallet/core/l10n/i10n.dart';
 import 'package:jetwallet/core/services/notification_service.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'dart:io' show Platform;
 
 Future<void> launchURL(BuildContext context, String url) async {
   final uri = Uri.parse(url);
 
   print(uri);
 
-  try {
-    await launchUrlString(url, mode: LaunchMode.externalApplication);
-  } catch (e) {
+  if (!await launchUrl(uri, mode: Platform.isAndroid ? LaunchMode.inAppWebView : LaunchMode.platformDefault)) {
     sNotification.showError('${intl.launchUrl_couldNotLaunch} $url');
+    throw Exception('Could not launch $url');
   }
 }
