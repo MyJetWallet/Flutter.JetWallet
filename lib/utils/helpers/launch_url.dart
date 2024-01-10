1d627f38149b502157d725ee5ad4535c6e2938b9
Dmitry <graviohz@gmail.com>
2024-01-10 10:23:58 +0200

    SPU-4103

--- lib/utils/helpers/launch_url.dart
+++ lib/utils/helpers/launch_url.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jetwallet/core/l10n/i10n.dart';
+import 'package:jetwallet/core/router/app_router.dart';
 import 'package:jetwallet/core/services/notification_service.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
@@ -8,10 +9,18 @@ import 'dart:io' show Platform;
 Future<void> launchURL(BuildContext context, String url) async {
   final uri = Uri.parse(url);
 
-  print(uri);
-
-  if (!await launchUrl(uri, mode: Platform.isAndroid ? LaunchMode.inAppWebView : LaunchMode.platformDefault)) {
-    sNotification.showError('${intl.launchUrl_couldNotLaunch} $url');
-    throw Exception('Could not launch $url');
+  if (Platform.isAndroid) {
+    launchUrlWevView(url);
+  } else {
+    if (!await launchUrl(uri, mode: Platform.isAndroid ? LaunchMode.inAppWebView : LaunchMode.platformDefault)) {
+      sNotification.showError('${intl.launchUrl_couldNotLaunch} $url');
+      throw Exception('Could not launch $url');
+    }
   }
+}
+
+void launchUrlWevView(String url) {
+  sRouter.push(
+    HelpCenterWebViewRouter(link: url, title: ''),
+  );
 }
