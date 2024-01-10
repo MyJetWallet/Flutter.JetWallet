4af7146a509024febf46e63cd90351cfd4a0567d
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-01-11 00:54:39 +0200

    changed deviceBindingRequired flow

--- lib/features/transfer_flow/store/transfer_confirmation_store.dart
+++ lib/features/transfer_flow/store/transfer_confirmation_store.dart
@@ -4,19 +4,15 @@ import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:jetwallet/core/l10n/i10n.dart';
 import 'package:jetwallet/core/router/app_router.dart';
-import 'package:jetwallet/core/services/remote_config/remote_config_values.dart';
 import 'package:jetwallet/core/services/signal_r/signal_r_service_new.dart';
 import 'package:jetwallet/core/services/simple_networking/simple_networking.dart';
-import 'package:jetwallet/core/services/user_info/user_info_service.dart';
-import 'package:jetwallet/features/phone_verification/ui/phone_verification.dart';
+import 'package:jetwallet/utils/device_binding_required_flow/show_device_binding_required_flow.dart';
 import 'package:jetwallet/utils/formatting/base/volume_format.dart';
-import 'package:jetwallet/utils/helpers/country_code_by_user_register.dart';
 import 'package:jetwallet/utils/helpers/navigate_to_router.dart';
 import 'package:jetwallet/utils/helpers/non_indices_with_balance_from.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_kit/modules/shared/stack_loader/store/stack_loader_store.dart';
-import 'package:simple_kit/simple_kit.dart';
 import 'package:simple_networking/helpers/models/server_reject_exception.dart';
 import 'package:simple_networking/modules/signal_r/models/banking_profile_model.dart';
 import 'package:simple_networking/modules/wallet_api/models/transfer/account_transfer_preview_request_model.dart';
@@ -167,54 +163,14 @@ abstract class _TransferConfirmationStoreBase with Store {
   Future<void> confirmTransfer() async {
     try {
       if (deviceBindingRequired) {
-        var continueBuying = false;
-
-        final formatedAmaunt = volumeFormat(
-          symbol: eurCurrency.symbol,
-          accuracy: eurCurrency.accuracy,
-          decimal: fromAmount,
-        );
-        await Future.delayed(const Duration(milliseconds: 500));
-        await sShowAlertPopup(
-          sRouter.navigatorKey.currentContext!,
-          primaryText: '',
-          secondaryText:
-              '${intl.binding_phone_dialog_first_part} $formatedAmaunt ${intl.binding_phone_dialog_second_part} $simpleCompanyName',
-          primaryButtonName: intl.binding_phone_dialog_confirm,
-          secondaryButtonName: intl.binding_phone_dialog_cancel,
-          image: Image.asset(
-            infoLightAsset,
-            width: 80,
-            height: 80,
-            package: 'simple_kit',
-          ),
-          onPrimaryButtonTap: () {
-            continueBuying = true;
-            sRouter.pop();
-          },
-          onSecondaryButtonTap: () {
-            continueBuying = false;
-            sRouter.pop();
-          },
-        );
-
-        if (!continueBuying) return;
-
-        final phoneNumber = countryCodeByUserRegister();
         var isVerifaierd = false;
-        await sRouter.push(
-          PhoneVerificationRouter(
-            args: PhoneVerificationArgs(
-              isDeviceBinding: true,
-              phoneNumber: sUserInfo.phone,
-              activeDialCode: phoneNumber,
-              onVerified: () {
-                isVerifaierd = true;
-                sRouter.pop();
-              },
-            ),
-          ),
+
+        await showDeviceBindingRequiredFlow(
+          onConfirmed: () {
+            isVerifaierd = true;
+          },
         );
+
         if (!isVerifaierd) return;
       }
 
