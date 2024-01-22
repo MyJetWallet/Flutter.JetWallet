2abce054f4a39daa4e2ce6dfba37d12de43e1614
Yaroslav Polishchuk <84226629+40rty5ive@users.noreply.github.com>
2024-01-22 16:41:16 +0200

    Fixed 4180 4188 4189 (#1389)
    * fixed 4188
    
    * fixed 4189
    
    * fixed 4180

--- lib/features/auth/single_sign_in/ui/sing_in.dart
+++ lib/features/auth/single_sign_in/ui/sing_in.dart
@@ -67,14 +67,12 @@ class _SingInBody extends StatelessObserverWidget {
 
               sNotification.showError(
                 (signInStore.union as Error).error.toString(),
-                hideIcon: true,
               );
             } else if (result is ErrorSrting) {
               signInStore.loader.finishLoading();
 
               sNotification.showError(
                 (signInStore.union as ErrorSrting).error!,
-                hideIcon: true,
               );
             } else if (result is Success) {
               signInStore.loader.finishLoading();
