b67a714afaeef553f1fe7ec2cb164edff108b386
Dmitry <graviohz@gmail.com>
2024-01-23 16:10:11 +0700

    fix push

--- lib/features/auth/email_verification/store/email_verification_store.dart
+++ lib/features/auth/email_verification/store/email_verification_store.dart
@@ -245,9 +245,10 @@ abstract class _EmailVerificationStoreBase with Store {
 
           authInfo.setAuthStatus(const AuthorizationUnion.authorized());
 
-          if (await Permission.notification.isDenied || await Permission.notification.isPermanentlyDenied) {
-            getIt.get<StartupService>().successfullAuthentication(needPush: false);
-          } else if (!await Permission.notification.isGranted) {
+          //if (await Permission.notification.isDenied || await Permission.notification.isPermanentlyDenied) {
+          getIt.get<StartupService>().successfullAuthentication(needPush: false);
+          //} else
+          if (!await Permission.notification.isGranted) {
             await sRouter.replace(const PushPermissionRoute());
           } else {
             getIt.get<StartupService>().successfullAuthentication(needPush: false);
