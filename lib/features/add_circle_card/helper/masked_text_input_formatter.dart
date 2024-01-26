a6366bd5920d310d9ba1fa251f42549e1a0e48d2
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-01-26 19:41:18 +0200

    fixed 4173 paste

--- lib/features/add_circle_card/helper/masked_text_input_formatter.dart
+++ lib/features/add_circle_card/helper/masked_text_input_formatter.dart
@@ -45,6 +45,10 @@ class MaskedTextInputFormatter extends TextInputFormatter {
     }
 
     // cases when user pastes text and oldValue is not empty
+    if (oldValue.text.isEmpty) {
+      return _value(mask, separator, newValue.text.replaceAll(RegExp(r'\s+\b|\b\s'), ''));
+    }
+
     return oldValue;
   }
 
