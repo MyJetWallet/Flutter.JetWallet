fae62a5dac9249703f954bb85b009ede1eee7287
asolodchenko <asolodchenko@outlook.com>
2024-02-12 14:26:55 +0100

    fix: chore

--- lib/features/earn/store/earn_store.dart
+++ lib/features/earn/store/earn_store.dart
@@ -4,6 +4,7 @@ import 'package:jetwallet/core/services/signal_r/signal_r_service_new.dart';
 import 'package:jetwallet/features/app/store/app_store.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
+import 'package:simple_networking/modules/signal_r/models/active_earn_positions_model.dart';
 import 'package:simple_networking/modules/signal_r/models/earn_offers_model_new.dart';
 
 part 'earn_store.g.dart';
@@ -15,6 +16,9 @@ class EarnStore extends _EarnStoreBase with _$EarnStore {
 }
 
 abstract class _EarnStoreBase with Store {
+  @computed
+  List<EarnPositionClientModel> get earnPositions => sSignalRModules.activeEarnPositionsMessage?.positions ?? [];
+
   /// Reflects the best (necessary) offers [EarnOfferClientModel] with
   ///  the status EarnOffers.Promotion == true
   @computed
@@ -25,9 +29,6 @@ abstract class _EarnStoreBase with Store {
           )
           .toList() ??
       [];
-
-  @computed
-  List<EarnOfferClientModel> get earnPositions => sSignalRModules.activeEarnPositionsMessage?.positions ?? [];
 
   @computed
   bool get isBalanceHide => !getIt<AppStore>().isBalanceHide;
