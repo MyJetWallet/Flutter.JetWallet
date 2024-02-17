df8762eb909edbdd49cdd13a5ce9769905cb55a7
asolodchenko <asolodchenko@outlook.com>
2024-02-17 08:41:07 +0100

    fix: pinn appbars

--- lib/features/earn/screens/earns_archive_screen.dart
+++ lib/features/earn/screens/earns_archive_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:jetwallet/core/l10n/i10n.dart';
 import 'package:jetwallet/features/earn/widgets/deposit_card.dart';
+import 'package:simple_kit/core/simple_kit.dart';
 import 'package:simple_kit_updated/widgets/navigation/top_app_bar/global_basic_appbar.dart';
 import 'package:simple_kit_updated/widgets/table/placeholder/simple_placeholder.dart';
 import 'package:simple_networking/modules/signal_r/models/active_earn_positions_model.dart';
@@ -13,25 +14,47 @@ class EarnsArchiveScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = sKit.colors;
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            GlobalBasicAppBar(
-              hasRightIcon: false,
-              title: intl.earn_earns_archive,
-            ),
-            if (earnPositionsClosed.isEmpty)
-              SPlaceholder(
-                size: SPlaceholderSize.l,
-                text: intl.wallet_simple_account_empty,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, _) {
+          return [
+            SliverAppBar(
+              backgroundColor: colors.white,
+              pinned: true,
+              elevation: 0,
+              expandedHeight: 120,
+              collapsedHeight: 120,
+              primary: false,
+              flexibleSpace: GlobalBasicAppBar(
+                hasRightIcon: false,
+                title: intl.earn_earns_archive,
               ),
-            ...earnPositionsClosed
-                .map(
-                  (e) => SDepositCard(earnPosition: e, onTap: () {}),
-                )
-                .toList(),
-            SizedBox(height: MediaQuery.paddingOf(context).bottom),
+            ),
+          ];
+        },
+        body: CustomScrollView(
+          slivers: [
+            if (earnPositionsClosed.isEmpty)
+              SliverFillRemaining(
+                child: SPlaceholder(
+                  size: SPlaceholderSize.l,
+                  text: intl.wallet_simple_account_empty,
+                ),
+              ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return SDepositCard(
+                    earnPosition: earnPositionsClosed[index],
+                    onTap: () {},
+                  );
+                },
+                childCount: earnPositionsClosed.length,
+              ),
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: MediaQuery.paddingOf(context).bottom)),
           ],
         ),
       ),
--- lib/features/earn/screens/offers_screen.dart
+++ lib/features/earn/screens/offers_screen.dart
@@ -4,6 +4,7 @@ import 'package:jetwallet/core/l10n/i10n.dart';
 import 'package:jetwallet/features/earn/store/earn_store.dart';
 import 'package:jetwallet/features/earn/widgets/earn_offers_list.dart';
 import 'package:provider/provider.dart';
+import 'package:simple_kit/core/simple_kit.dart';
 import 'package:simple_kit_updated/widgets/navigation/top_app_bar/global_basic_appbar.dart';
 
 @RoutePage(name: 'OffersRouter')
@@ -17,20 +18,37 @@ class OffersScreen extends StatelessWidget {
       builder: (context, child) {
         final store = EarnStore.of(context);
         final offers = store.earnOffers;
+        final colors = sKit.colors;
 
         return Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                GlobalBasicAppBar(
-                  hasRightIcon: false,
-                  title: intl.earn_all_offers,
+          body: NestedScrollView(
+            headerSliverBuilder: (context, _) {
+              return [
+                SliverAppBar(
+                  backgroundColor: colors.white,
+                  pinned: true,
+                  elevation: 0,
+                  expandedHeight: 120,
+                  collapsedHeight: 120,
+                  primary: false,
+                  flexibleSpace: GlobalBasicAppBar(
+                    hasRightIcon: false,
+                    title: intl.earn_all_offers,
+                  ),
                 ),
-                OffersListWidget(
-                  showTitle: false,
-                  earnOffers: offers,
+              ];
+            },
+            body: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: OffersListWidget(
+                    showTitle: false,
+                    earnOffers: offers,
+                  ),
                 ),
-                SizedBox(height: MediaQuery.paddingOf(context).bottom),
+                SliverToBoxAdapter(
+                  child: SizedBox(height: MediaQuery.of(context).padding.bottom),
+                ),
               ],
             ),
           ),
