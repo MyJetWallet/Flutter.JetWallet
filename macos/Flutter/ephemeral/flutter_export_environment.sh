c9d5dfb112663b1f26a9f3efeed3a6a7eae7df88
asolodchenko <asolodchenko@outlook.com>
2024-02-29 13:05:41 +0100

    fix: update versions

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/user/Downloads/flutter_new"
-export "FLUTTER_APPLICATION_PATH=/Users/user/AndroidStudioProjects/Flutter.JetWallet"
+export "FLUTTER_ROOT=/Users/darkdezert/fvm/versions/3.13.9"
+export "FLUTTER_APPLICATION_PATH=/Users/darkdezert/Desktop/Flutter/Flutter.JetWallet"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=3.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
