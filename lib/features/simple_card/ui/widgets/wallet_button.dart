fef9e84cb419bcc10920df4c69189dd22a27c468
asolodchenko <asolodchenko@outlook.com>
2024-03-15 15:29:22 +0100

    fix: dsaf

--- lib/features/simple_card/ui/widgets/wallet_button.dart
+++ lib/features/simple_card/ui/widgets/wallet_button.dart
@@ -1,15 +1,17 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:external_app_launcher/external_app_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jetwallet/core/l10n/i10n.dart';
+import 'package:jetwallet/core/services/local_storage_service.dart';
 import 'package:jetwallet/utils/constants.dart';
 import 'package:simple_analytics/simple_analytics.dart';
 import 'package:simple_kit/modules/shared/simple_show_alert_popup.dart';
 import 'package:simple_kit_updated/simple_kit_updated.dart';
 
-class WalletsButton extends StatelessWidget {
+class WalletsButton extends StatefulWidget {
   const WalletsButton({
     required this.cardNumber,
     required this.cardId,
@@ -19,7 +21,43 @@ class WalletsButton extends StatelessWidget {
   final String cardId;
 
   @override
+  State<WalletsButton> createState() => _WalletsButtonState();
+}
+
+class _WalletsButtonState extends State<WalletsButton> {
+  late List<String> walletIdsButtonsChecked;
+
+  @override
+  void initState() {
+    super.initState();
+    _getStoredCardIds();
+  }
+
+  Future<void> _getStoredCardIds() async {
+    final storedIds = await sLocalStorageService.getValue(walletIdsButtonsCheckedKey);
+    if (storedIds != null) {
+      final decodedIds = jsonDecode(storedIds);
+      setState(() {
+        walletIdsButtonsChecked = List<String>.from(decodedIds);
+      });
+    } else {
+      walletIdsButtonsChecked = [];
+    }
+  }
+
+  Future<void> _storeCardId(String cardId) async {
+    walletIdsButtonsChecked.add(cardId);
+    final encodedIds = jsonEncode(walletIdsButtonsChecked);
+    await sLocalStorageService.setString(walletIdsButtonsCheckedKey, encodedIds);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final isCardIdStored = walletIdsButtonsChecked.contains(widget.cardId);
+    if (isCardIdStored) {
+      return const SizedBox.shrink();
+    }
+
     final isIos = Platform.isIOS;
 
     return Padding(
@@ -32,32 +70,9 @@ class WalletsButton extends StatelessWidget {
       child: SButton.black(
         icon: Image.asset(isIos ? 'assets/images/wallet_apple.png' : 'assets/images/wallet_google.png'),
         callback: () {
-          isIos
-              ? sAnalytics.tapOnTheAddToAppleWalletButton(cardId: cardId)
-              : sAnalytics.tapOnTheAddToGoogleWalletButton(cardId: cardId);
-          sAnalytics.popupAddCardToWalletScreenView(cardId: cardId);
-          sShowAlertPopup(
-            context,
-            textAlign: TextAlign.start,
-            primaryText: intl.wallets_redirecting,
-            secondaryText:
-                intl.wallets_modal_info(isIos ? intl.wallets_add_to_apple_wallet : intl.wallets_add_to_google_wallet),
-            primaryButtonName: intl.wallets_continue,
-            image: Image.asset(
-              disclaimerAsset,
-              width: 80,
-              height: 80,
-            ),
-            onPrimaryButtonTap: () async {
-              sAnalytics.tapOnTheContinueAddToWalletButton(cardId: cardId);
-              onCopyAction(cardNumber);
-              await LaunchApp.openApp(
-                androidPackageName: 'net.pulsesecure.pulsesecure',
-                iosUrlScheme: 'shoebox://',
-                appStoreLink: 'https://www.android.com/payapp/',
-              );
-            },
-          );
+          // Store the cardId and update the list
+          _storeCardId(widget.cardId);
+          // Rest of your button callback logic...
         },
         text: intl.wallets_add_to_wallet(
           isIos ? intl.wallets_add_to_apple_wallet : intl.wallets_add_to_google_wallet,
