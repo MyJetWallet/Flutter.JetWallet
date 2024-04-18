882c355e09e988938789697c0a33d4e9a3d777b7
asolodchenko <asolodchenko@outlook.com>
2024-04-18 18:01:02 +0200

    fix: invest navigation

--- lib/features/invest/invest_screen.dart
+++ lib/features/invest/invest_screen.dart
@@ -165,22 +165,12 @@ class _InvestScreenState extends State<InvestScreen> {
                                         profit: getGroupedProfit(instrument.symbol ?? ''),
                                         price: investStore.getPriceBySymbol(instrument.symbol ?? ''),
                                         onTap: () {
-                                          final positions = investPositionsStore.activeList
-                                              .where((element) => element.symbol == instrument.symbol)
-                                              .toList();
-
-                                          if (positions.length > 1) {
-                                            sRouter.push(
-                                              InstrumentPageRouter(instrument: instrument),
-                                            );
-                                          } else {
-                                            sRouter.push(
-                                              ActiveInvestManageRouter(
-                                                instrument: instrument,
-                                                position: positions.first,
-                                              ),
-                                            );
-                                          }
+                                          // final positions = investPositionsStore.activeList
+                                          //     .where((element) => element.symbol == instrument.symbol)
+                                          //     .toList();
+                                          sRouter.push(
+                                            InstrumentPageRouter(instrument: instrument),
+                                          );
                                         },
                                         candles: investChartStore.getAssetCandles(instrument.symbol ?? ''),
                                       ),
