d23730e508f7442ee0114d4508422ac3971617f2
asolodchenko <asolodchenko@outlook.com>
2024-04-25 15:11:26 +0200

    fix: st tp 0 validaiton

--- lib/features/invest/stores/dashboard/invest_new_store.dart
+++ lib/features/invest/stores/dashboard/invest_new_store.dart
@@ -1006,6 +1006,24 @@ abstract class _InvestNewStoreBase with Store {
 
     final newPendingValue = pendingValue == Decimal.zero ? marketPrice : pendingValue;
 
+    if (slAmountValue == Decimal.zero) {
+      sNotification.showError(
+        '${intl.invest_error_sl_zero}0',
+        id: 1,
+        needFeedback: true,
+      );
+      return true;
+    }
+
+    if (tpAmountValue == Decimal.zero) {
+      sNotification.showError(
+        '${intl.invest_error_tp_zero}0',
+        id: 1,
+        needFeedback: true,
+      );
+      return true;
+    }
+
     if (newPendingValue < minPriceMarket) {
       sNotification.showError(
         '${intl.invest_error_pending_price} $minPriceMarket - $maxPriceMarket',
--- lib/features/invest/ui/instrument_screen.dart
+++ lib/features/invest/ui/instrument_screen.dart
@@ -76,11 +76,6 @@ class _InstrumentScreenState extends State<InstrumentScreen> {
     final colors = sKit.colors;
     final currency = currencyFrom(currencies, 'USDT');
 
-    final activeListToShow = investPositionsStore.activeList
-        .where(
-          (element) => element.symbol == widget.instrument.symbol,
-        )
-        .toList();
     return SPageFrame(
       loaderText: intl.register_pleaseWait,
       loading: investPositionsStore.loader,
