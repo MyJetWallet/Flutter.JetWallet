2d9cbc4c51cce55ae8ee7aa389802bcb0a8f5fc8
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-05-09 23:43:21 +0300

    ашчув 4644

--- lib/features/currency_buy/ui/screens/pay_with_bottom_sheet.dart
+++ lib/features/currency_buy/ui/screens/pay_with_bottom_sheet.dart
@@ -3,17 +3,12 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:jetwallet/core/di/di.dart';
 import 'package:jetwallet/core/l10n/i10n.dart';
 import 'package:jetwallet/core/router/app_router.dart';
-import 'package:jetwallet/core/services/notification_service.dart';
 import 'package:jetwallet/core/services/signal_r/signal_r_service_new.dart';
 import 'package:jetwallet/features/app/store/app_store.dart';
-import 'package:jetwallet/features/bank_card/add_bank_card.dart';
 import 'package:jetwallet/features/buy_flow/store/payment_method_store.dart';
 import 'package:jetwallet/features/buy_flow/ui/amount_screen.dart';
 import 'package:jetwallet/features/buy_flow/ui/widgets/payment_methods_widgets/payment_method_cards_widget.dart';
 import 'package:jetwallet/features/cj_banking_accounts/widgets/show_add_cash_from_bottom_sheet.dart';
-import 'package:jetwallet/features/kyc/helper/kyc_alert_handler.dart';
-import 'package:jetwallet/features/kyc/kyc_service.dart';
-import 'package:jetwallet/features/kyc/models/kyc_operation_status_model.dart';
 import 'package:jetwallet/utils/balances/crypto_balance.dart';
 import 'package:jetwallet/utils/models/currency_model.dart';
 import 'package:jetwallet/widgets/action_bottom_sheet_header.dart';
@@ -21,7 +16,6 @@ import 'package:simple_analytics/simple_analytics.dart';
 import 'package:simple_kit/simple_kit.dart';
 import 'package:simple_kit_updated/gen/assets.gen.dart';
 import 'package:simple_kit_updated/simple_kit_updated.dart';
-import 'package:simple_networking/modules/signal_r/models/asset_payment_methods.dart';
 import 'package:simple_networking/modules/signal_r/models/banking_profile_model.dart';
 import 'package:simple_networking/modules/wallet_api/models/circle_card.dart';
 
@@ -43,76 +37,51 @@ void showPayWithBottomSheet({
       hideCards: hideCards,
     );
 
-  if (store.cards.isNotEmpty || store.accounts.isNotEmpty) {
-    sAnalytics.payWithPMSheetView(
-      destinationWallet: currency?.symbol ?? '',
-      listOfAvailablePMs: [
-        if (store.isCardsAvailable) ...[
-          'New card',
-          ...List.generate(
-            store.cards.length,
-            (index) => 'Saved card ${store.cards[index].last4}',
-          ),
-        ],
-        if (store.isBankingAccountsAvaible)
-          ...List.generate(
-            store.accounts.length,
-            (index) {
-              return index == 0
-                  ? 'CJ ${store.accounts[index].last4IbanCharacters}'
-                  : 'Unlimint ${store.accounts[index].last4IbanCharacters}';
-            },
-          ),
-      ],
-    );
-
-    sShowBasicModalBottomSheet(
-      context: context,
-      then: (value) {
-        sAnalytics.tapOnTheButtonCloseForClosingSheetOnPayWithPMSheet(
-          destinationWallet: currency?.symbol ?? '',
-        );
-      },
-      scrollable: true,
-      pinned: ActionBottomSheetHeader(
-        name: intl.amount_screen_pay_with,
-        needBottomPadding: false,
-      ),
-      horizontalPinnedPadding: 0.0,
-      removePinnedPadding: true,
-      children: [
-        _PaymentMethodScreenBody(
-          asset: currency,
-          onSelected: onSelected,
-          store: store,
-          onSelectedCryptoAsset: onSelectedCryptoAsset,
+  sAnalytics.payWithPMSheetView(
+    destinationWallet: currency?.symbol ?? '',
+    listOfAvailablePMs: [
+      if (store.isCardsAvailable) ...[
+        'New card',
+        ...List.generate(
+          store.cards.length,
+          (index) => 'Saved card ${store.cards[index].last4}',
         ),
       ],
-    );
-  } else {
-    final kycState = getIt.get<KycService>();
-    final kycHandler = getIt.get<KycAlertHandler>();
+      if (store.isBankingAccountsAvaible)
+        ...List.generate(
+          store.accounts.length,
+          (index) {
+            return index == 0
+                ? 'CJ ${store.accounts[index].last4IbanCharacters}'
+                : 'Unlimint ${store.accounts[index].last4IbanCharacters}';
+          },
+        ),
+    ],
+  );
 
-    final isCardsAvailable = sSignalRModules.buyMethods.any((element) => element.id == PaymentMethodType.bankCard);
-
-    final status = kycOperationStatus(KycStatus.allowed);
-    if (!isCardsAvailable) {
-      sNotification.showError(
-        intl.operation_bloked_text,
-        id: 1,
+  sShowBasicModalBottomSheet(
+    context: context,
+    then: (value) {
+      sAnalytics.tapOnTheButtonCloseForClosingSheetOnPayWithPMSheet(
+        destinationWallet: currency?.symbol ?? '',
       );
-    } else if (kycState.depositStatus == status) {
-      _onAddCardTap(context, currency);
-    } else {
-      kycHandler.handle(
-        status: kycState.depositStatus,
-        isProgress: kycState.verificationInProgress,
-        currentNavigate: () => _onAddCardTap(context, currency),
-        requiredDocuments: kycState.requiredDocuments,
-        requiredVerifications: kycState.requiredVerifications,
-      );
-    }
-  }
+    },
+    scrollable: true,
+    pinned: ActionBottomSheetHeader(
+      name: intl.amount_screen_pay_with,
+      needBottomPadding: false,
+    ),
+    horizontalPinnedPadding: 0.0,
+    removePinnedPadding: true,
+    children: [
+      _PaymentMethodScreenBody(
+        asset: currency,
+        onSelected: onSelected,
+        store: store,
+        onSelectedCryptoAsset: onSelectedCryptoAsset,
+      ),
+    ],
+  );
 }
 
 class _PaymentMethodScreenBody extends StatelessObserverWidget {
@@ -223,35 +192,4 @@ class _PaymentMethodScreenBody extends StatelessObserverWidget {
       ),
     );
   }
-}
-
-void _onAddCardTap(BuildContext context, CurrencyModel? asset) {
-  Navigator.push(
-    context,
-    PageRouteBuilder(
-      opaque: false,
-      barrierColor: Colors.white,
-      pageBuilder: (BuildContext _, __, ___) {
-        return AddBankCardScreen(
-          onCardAdded: () {},
-          amount: '',
-          isPreview: true,
-          asset: asset,
-          divideDateAndLabel: true,
-        );
-      },
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(0.0, 1.0);
-        const end = Offset.zero;
-        const curve = Curves.ease;
-
-        final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    ),
-  );
 }
