bdcdcfb8311a0d5ad7923c988aef11f0ae657847
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-05-14 01:52:00 +0300

    fixed 4649

--- lib/features/actions/action_sell/action_sell.dart
+++ lib/features/actions/action_sell/action_sell.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:jetwallet/core/di/di.dart';
 import 'package:jetwallet/core/router/app_router.dart';
+import 'package:jetwallet/features/actions/action_send/widgets/show_send_timer_alert_or.dart';
 import 'package:jetwallet/features/buy_flow/ui/amount_screen.dart';
 import 'package:jetwallet/features/kyc/helper/kyc_alert_handler.dart';
 import 'package:jetwallet/features/kyc/kyc_service.dart';
 import 'package:jetwallet/features/sell_flow/widgets/sell_choose_asset_bottom_sheet.dart';
 import 'package:jetwallet/features/sell_flow/widgets/sell_with_bottom_sheet.dart';
 import 'package:simple_analytics/simple_analytics.dart';
+import 'package:simple_networking/modules/signal_r/models/client_detail_model.dart';
 
 void showSellAction(BuildContext context) {
   final kyc = getIt.get<KycService>();
   final handler = getIt.get<KycAlertHandler>();
+
   handler.handle(
+    status: kyc.tradeStatus,
     isProgress: kyc.verificationInProgress,
-    currentNavigate: () => _sellAction(context),
+    currentNavigate: () => showSendTimerAlertOr(
+      context: context,
+      or: () {
+        _sellAction(context);
+      },
+      from: [BlockingType.trade],
+    ),
     requiredDocuments: kyc.requiredDocuments,
     requiredVerifications: kyc.requiredVerifications,
   );
