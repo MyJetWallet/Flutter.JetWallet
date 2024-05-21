0437632dae5f4b2aa60ae513e62692a06664f24b
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-05-21 18:55:53 +0300

    ашчув 4675

--- lib/core/services/deep_link_service.dart
+++ lib/core/services/deep_link_service.dart
@@ -736,12 +736,14 @@ class DeepLinkService {
         getIt.get<AppStore>().remoteConfigStatus is Success &&
         getIt.get<AppStore>().authorizedStatus is Home) {
       unawaited(sRouter.push(const AccountRouter()));
+      await Future.delayed(const Duration(microseconds: 650));
       await poshToScreenInProfileScreen(symbol);
     } else {
       getIt<RouteQueryService>().addToQuery(
         RouteQueryModel(
           func: () async {
             unawaited(sRouter.push(const AccountRouter()));
+            await Future.delayed(const Duration(microseconds: 650));
             await poshToScreenInProfileScreen(symbol);
           },
         ),
