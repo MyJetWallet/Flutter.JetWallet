37849af33680e38d9c15c673bd04bac1552cc541
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-07-08 19:35:31 +0300

    some fixes

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/apoli/fvm/versions/3.19.2"
+export "FLUTTER_ROOT=/Users/apoli/fvm/versions/3.22.2"
 export "FLUTTER_APPLICATION_PATH=/Users/apoli/Documents/GitHub/flutter_jet_wallet"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
