8fa35ba1b8a3f942a469c5d4376e3b2d23115521
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-07-25 23:36:57 +0300

    fixed 4769 withdrawal failed screen

--- lib/features/iban/iban_send/iban_send_confirm/store/iban_send_confirm_store.dart
+++ lib/features/iban/iban_send/iban_send_confirm/store/iban_send_confirm_store.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:jetwallet/core/di/di.dart';
@@ -17,6 +19,7 @@ import 'package:provider/provider.dart';
 import 'package:simple_analytics/simple_analytics.dart';
 import 'package:simple_kit/modules/shared/stack_loader/store/stack_loader_store.dart';
 import 'package:simple_kit/simple_kit.dart';
+import 'package:simple_networking/helpers/models/server_reject_exception.dart';
 import 'package:simple_networking/modules/signal_r/models/banking_profile_model.dart';
 import 'package:simple_networking/modules/wallet_api/models/address_book/address_book_model.dart';
 import 'package:simple_networking/modules/wallet_api/models/banking_withdrawal/banking_withdrawal_preview_model.dart';
@@ -61,81 +64,105 @@ abstract class _IbanSendConfirmStoreBase with Store {
     SimpleBankingAccount account,
     bool isCJ,
   ) async {
-    if (deviceBindingRequired) {
-      var continueBuying = false;
+    try {
+      if (deviceBindingRequired) {
+        var continueBuying = false;
 
-      final formatedAmaunt = volumeFormat(
-        symbol: eurCurrency.symbol,
-        accuracy: eurCurrency.accuracy,
-        decimal: data.amount ?? Decimal.fromInt(200),
-      );
-      await Future.delayed(const Duration(milliseconds: 500));
-      await sShowAlertPopup(
-        sRouter.navigatorKey.currentContext!,
-        primaryText: '',
-        secondaryText:
-            '${intl.binding_phone_dialog_first_part_2} $formatedAmaunt ${intl.binding_phone_dialog_second_part_2}',
-        primaryButtonName: intl.binding_phone_dialog_confirm,
-        secondaryButtonName: intl.binding_phone_dialog_cancel,
-        image: Image.asset(
-          infoLightAsset,
-          width: 80,
-          height: 80,
-          package: 'simple_kit',
-        ),
-        onPrimaryButtonTap: () {
-          continueBuying = true;
-          sRouter.maybePop();
-        },
-        onSecondaryButtonTap: () {
-          continueBuying = false;
-          sRouter.maybePop();
-        },
-      );
-
-      if (!continueBuying) return;
-
-      final phoneNumber = countryCodeByUserRegister();
-      var isVerifaierd = false;
-      await sRouter.push(
-        PhoneVerificationRouter(
-          args: PhoneVerificationArgs(
-            isDeviceBinding: true,
-            phoneNumber: sUserInfo.phone,
-            activeDialCode: phoneNumber,
-            onVerified: () {
-              isVerifaierd = true;
-              sRouter.maybePop();
-            },
+        final formatedAmaunt = volumeFormat(
+          symbol: eurCurrency.symbol,
+          accuracy: eurCurrency.accuracy,
+          decimal: data.amount ?? Decimal.fromInt(200),
+        );
+        await Future.delayed(const Duration(milliseconds: 500));
+        await sShowAlertPopup(
+          sRouter.navigatorKey.currentContext!,
+          primaryText: '',
+          secondaryText:
+              '${intl.binding_phone_dialog_first_part_2} $formatedAmaunt ${intl.binding_phone_dialog_second_part_2}',
+          primaryButtonName: intl.binding_phone_dialog_confirm,
+          secondaryButtonName: intl.binding_phone_dialog_cancel,
+          image: Image.asset(
+            infoLightAsset,
+            width: 80,
+            height: 80,
+            package: 'simple_kit',
           ),
-        ),
+          onPrimaryButtonTap: () {
+            continueBuying = true;
+            sRouter.maybePop();
+          },
+          onSecondaryButtonTap: () {
+            continueBuying = false;
+            sRouter.maybePop();
+          },
+        );
+
+        if (!continueBuying) return;
+
+        final phoneNumber = countryCodeByUserRegister();
+        var isVerifaierd = false;
+        await sRouter.push(
+          PhoneVerificationRouter(
+            args: PhoneVerificationArgs(
+              isDeviceBinding: true,
+              phoneNumber: sUserInfo.phone,
+              activeDialCode: phoneNumber,
+              onVerified: () {
+                isVerifaierd = true;
+                sRouter.maybePop();
+              },
+            ),
+          ),
+        );
+        if (!isVerifaierd) return;
+      }
+
+      loader.startLoadingImmediately();
+
+      final model = BankingWithdrawalRequest(
+        pin: pin,
+        accountId: previewRequest.accountId,
+        requestId: requestId,
+        toIbanAddress: previewRequest.toIbanAddress,
+        contactId: previewRequest.contactId,
+        assetSymbol: 'EUR',
+        amount: data.amount,
+        description: previewRequest.description,
+        beneficiaryName: previewRequest.beneficiaryName,
+        beneficiaryAddress: previewRequest.beneficiaryAddress,
+        beneficiaryBankCode: previewRequest.beneficiaryBankCode,
+        beneficiaryCountry: previewRequest.beneficiaryCountry,
+        expressPayment: previewRequest.expressPayment,
       );
-      if (!isVerifaierd) return;
-    }
 
-    loader.startLoadingImmediately();
+      final response = await getIt.get<SNetwork>().simpleNetworking.getWalletModule().postBankingWithdrawal(model);
 
-    final model = BankingWithdrawalRequest(
-      pin: pin,
-      accountId: previewRequest.accountId,
-      requestId: requestId,
-      toIbanAddress: previewRequest.toIbanAddress,
-      contactId: previewRequest.contactId,
-      assetSymbol: 'EUR',
-      amount: data.amount,
-      description: previewRequest.description,
-      beneficiaryName: previewRequest.beneficiaryName,
-      beneficiaryAddress: previewRequest.beneficiaryAddress,
-      beneficiaryBankCode: previewRequest.beneficiaryBankCode,
-      beneficiaryCountry: previewRequest.beneficiaryCountry,
-      expressPayment: previewRequest.expressPayment,
-    );
+      loader.finishLoadingImmediately();
 
-    final response = await getIt.get<SNetwork>().simpleNetworking.getWalletModule().postBankingWithdrawal(model);
+      if (response.hasError) {
+        sAnalytics.eurWithdrawFailed(
+          isCJ: isCJ,
+          accountIban: account.iban ?? '',
+          accountLabel: account.label ?? '',
+          eurAccType: contact.iban ?? '',
+          eurAccLabel: contact.name ?? '',
+          enteredAmount: data.amount.toString(),
+        );
 
-    loader.finishLoadingImmediately();
+        await showFailureScreen(response.error?.cause ?? '');
+      } else {
+        sAnalytics.eurWithdrawSuccessWithdrawEndSV(
+          isCJ: isCJ,
+          accountIban: account.iban ?? '',
+          accountLabel: account.label ?? '',
+          eurAccType: contact.iban ?? '',
+          eurAccLabel: contact.name ?? '',
+          enteredAmount: data.amount.toString(),
+        );
 
-    if (response.hasError) {
+        await showSuccessScreen(data.sendAmount);
+      }
+    } on ServerRejectException catch (error) {
       sAnalytics.eurWithdrawFailed(
         isCJ: isCJ,
         accountIban: account.iban ?? '',
@@ -144,10 +171,9 @@ abstract class _IbanSendConfirmStoreBase with Store {
         eurAccLabel: contact.name ?? '',
         enteredAmount: data.amount.toString(),
       );
-
-      await showFailureScreen(response.error?.cause ?? '');
-    } else {
-      sAnalytics.eurWithdrawSuccessWithdrawEndSV(
+      unawaited(showFailureScreen(error.cause));
+    } catch (error) {
+      sAnalytics.eurWithdrawFailed(
         isCJ: isCJ,
         accountIban: account.iban ?? '',
         accountLabel: account.label ?? '',
@@ -155,8 +181,7 @@ abstract class _IbanSendConfirmStoreBase with Store {
         eurAccLabel: contact.name ?? '',
         enteredAmount: data.amount.toString(),
       );
-
-      await showSuccessScreen(data.sendAmount);
+      unawaited(showFailureScreen(intl.something_went_wrong));
     }
   }
 
