dc2f6635615b06e328b7b74ff94e8d9100421877
StanislavCh1 <stanislav.ch@simple.app>
2024-08-08 21:08:44 +0300

    Fix bug

--- plugins/simple_networking/lib/modules/remote_config/models/remote_config_appsflyer.dart
+++ plugins/simple_networking/lib/modules/remote_config/models/remote_config_appsflyer.dart
@@ -12,7 +12,7 @@ class RemoteConfigAppsflyer with _$RemoteConfigAppsflyer {
   factory RemoteConfigAppsflyer({
     required String devKey,
     required String iosAppId,
-    @Default('app.simple.com') required String androidAppId,
+    @Default('app.simple.com') String androidAppId,
   }) = _RemoteConfigAppsflyer;
 
   factory RemoteConfigAppsflyer.fromJson(Map<String, dynamic> json) => _$RemoteConfigAppsflyerFromJson(json);
