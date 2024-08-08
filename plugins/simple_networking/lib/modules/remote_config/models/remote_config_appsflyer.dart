f2aba7add19dc8c2c2a6d48a9a5066f7c060ebf3
StanislavCh1 <stanislav.ch@simple.app>
2024-08-08 20:21:29 +0300

    Temp fix prod build (add default value to androidAppId RemoteConfigAppsflyer)

--- plugins/simple_networking/lib/modules/remote_config/models/remote_config_appsflyer.dart
+++ plugins/simple_networking/lib/modules/remote_config/models/remote_config_appsflyer.dart
@@ -3,12 +3,16 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'remote_config_appsflyer.freezed.dart';
 part 'remote_config_appsflyer.g.dart';
 
+///
+/// TODO: delete default value!!!
+///
+
 @freezed
 class RemoteConfigAppsflyer with _$RemoteConfigAppsflyer {
   factory RemoteConfigAppsflyer({
     required String devKey,
     required String iosAppId,
-    required String androidAppId,
+    @Default('app.simple.com') required String androidAppId,
   }) = _RemoteConfigAppsflyer;
 
   factory RemoteConfigAppsflyer.fromJson(Map<String, dynamic> json) => _$RemoteConfigAppsflyerFromJson(json);
