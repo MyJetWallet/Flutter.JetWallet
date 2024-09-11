eb3e8df2ffc412c80e6528dfb4a95a78204faa73
StanislavCh1 <stanislav.ch@simple.app>
2024-09-11 16:15:30 +0300

    Fix withdrawal preview screen

--- lib/features/withdrawal/ui/withdrawal_preview.dart
+++ lib/features/withdrawal/ui/withdrawal_preview.dart
@@ -115,21 +115,14 @@ class _WithdrawalPreviewScreenState extends State<WithdrawalPreviewScreen> {
                     isLoading: false,
                     assetIconUrl: store.withdrawalInputModel!.currency!.iconUrl,
                     assetDescription: store.withdrawalInputModel!.currency!.description,
-                    assetValue: store.addressIsInternal
-                        ? Decimal.parse(store.withAmount).toFormatCount(
-                            accuracy: store.withdrawalInputModel!.currency!.accuracy,
-                            symbol: store.withdrawalInputModel!.currency!.symbol,
-                          )
-                        : (Decimal.parse(store.withAmount) + feeSize).toFormatCount(
-                            accuracy: store.withdrawalInputModel!.currency!.accuracy,
-                            symbol: store.withdrawalInputModel!.currency!.symbol,
-                          ),
+                    assetValue: (Decimal.parse(store.withAmount) + feeSize).toFormatCount(
+                      accuracy: store.withdrawalInputModel!.currency!.accuracy,
+                      symbol: store.withdrawalInputModel!.currency!.symbol,
+                    ),
                     assetBaseAmount: formatService
                         .convertOneCurrencyToAnotherOne(
                           fromCurrency: store.withdrawalInputModel!.currency!.symbol,
-                          fromCurrencyAmmount: store.addressIsInternal
-                              ? Decimal.parse(store.withAmount)
-                              : Decimal.parse(store.withAmount) + feeSize,
+                          fromCurrencyAmmount: Decimal.parse(store.withAmount) + feeSize,
                           toCurrency: sSignalRModules.baseCurrency.symbol,
                           baseCurrency: sSignalRModules.baseCurrency.symbol,
                           isMin: false,
