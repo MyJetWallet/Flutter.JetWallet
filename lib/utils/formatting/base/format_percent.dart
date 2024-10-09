ce5c20681a3e7f3bff7915276e515c1e6fd29cbc
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-10-09 23:41:35 +0300

    added documentation

--- lib/utils/formatting/base/format_percent.dart
+++ lib/utils/formatting/base/format_percent.dart
@@ -1,7 +1,13 @@
 extension FormatPercentExtension on double {
+  /// Formats a double value representing price change percentages
+  ///
+  /// Example:
+  /// 3.456 -> "+3.46%"
+  /// -1.234 -> "-1.23%"
+  /// 0.0 -> "0.00%"
   String toFormatPercentPriceChange() {
     if (compareTo(0) == 0) {
-      return '0.00%';
+      return '+0.00%';
     } else if (isNegative) {
       return '${toStringAsFixed(2)}%';
     } else {
@@ -9,6 +15,13 @@ extension FormatPercentExtension on double {
     }
   }
 
+  /// Formats a double value to a percentage representation
+  /// with trimmed unnecessary decimal places.
+  ///
+  /// Example:
+  /// 45.50 -> "45.5%"
+  /// -10.0 -> "-10%"
+  /// 0.0 -> "0%"
   String toFormatPercentCount() {
     return '${toStringAsFixed(2).replaceFirst(RegExp(r'\.?0*$'), '')}%';
   }
