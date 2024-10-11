6dec0f7d4ea0d0fcc62db508cf2e1bff4d473b36
StanislavCh1 <stanislav.ch@simple.app>
2024-10-11 11:37:06 +0300

    Add new regexp for email validation

--- lib/utils/helpers/string_helper.dart
+++ lib/utils/helpers/string_helper.dart
@@ -44,9 +44,8 @@ String localizedMonth(
 }
 
 bool isEmailValid(String email) {
-  const pattern = r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-      r'{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]'
-      r'{0,253}[a-zA-Z0-9])?)*$';
+  const pattern =
+      r'''^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$''';
 
   return RegExp(pattern).hasMatch(email);
 }
