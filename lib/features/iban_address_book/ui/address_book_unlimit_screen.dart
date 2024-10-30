126f2e5bae364753d58bc9a4cc86046ba1840c98
Yaroslav Polishchuk <apolisuk065@gmail.com>
2024-10-30 15:06:59 +0200

    fixed whire screen

--- lib/features/iban_address_book/ui/address_book_unlimit_screen.dart
+++ lib/features/iban_address_book/ui/address_book_unlimit_screen.dart
@@ -350,7 +350,9 @@ class _BodyAdressBookUnlimitState extends State<_BodyAdressBookUnlimit> {
                                   );
                                 }
 
-                                Navigator.pop(context, true);
+                                if (Navigator.canPop(context)) {
+                                  Navigator.pop(context, true);
+                                }
                               }
                             },
                           ),
