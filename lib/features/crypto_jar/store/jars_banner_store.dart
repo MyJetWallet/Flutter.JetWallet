ce65b7117b0fa0105d3694be18aee0069d006067
StanislavCh <stanislav.ch@simple.app>
2024-11-19 17:34:15 +0200

    Add temp jars landing

--- lib/features/crypto_jar/store/jars_banner_store.dart
+++ lib/features/crypto_jar/store/jars_banner_store.dart
@@ -27,7 +27,8 @@ abstract class _JarsBannerStoreBase with Store {
 
   @action
   Future<void> init() async {
-    landingUrl = jarsInfoLanding;
+    // landingUrl = jarsInfoLanding;
+    landingUrl = 'https://uat.simple-spot.biz/lp/jars/';
 
     isBannerClosed =
         bool.tryParse(await getIt.get<LocalStorageService>().getValue(isJarsLandingClosed) ?? 'false') ?? false;
