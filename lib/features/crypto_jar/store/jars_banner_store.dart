8fcb2b9e23b440b19422e707c37d7de3783bd4db
StanislavCh <stanislav.ch@simple.app>
2024-11-29 11:25:33 +0200

    Add jar landing language

--- lib/features/crypto_jar/store/jars_banner_store.dart
+++ lib/features/crypto_jar/store/jars_banner_store.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jetwallet/core/di/di.dart';
 import 'package:jetwallet/core/services/local_storage_service.dart';
 import 'package:jetwallet/core/services/remote_config/remote_config_values.dart';
+import 'package:jetwallet/features/app/store/app_store.dart';
 import 'package:jetwallet/utils/helpers/launch_url.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
@@ -40,8 +41,31 @@ abstract class _JarsBannerStoreBase with Store {
   }
 
   void openLanding(BuildContext context) {
-    if (landingUrl.isNotEmpty) {
-      launchURL(context, landingUrl, launchMode: LaunchMode.externalApplication);
+    var url = landingUrl;
+    if (landingUrl.contains('lp/jars')) {
+      final languageCode = getIt.get<AppStore>().locale?.languageCode;
+      if (languageCode == 'uk') {
+        url = addLocaleToUrl('ua');
+      } else if (languageCode == 'pl') {
+        url = addLocaleToUrl('pl');
+      }
+      print('#@#@#@ $languageCode $url');
     }
+    if (url.isNotEmpty) {
+      launchURL(context, url, launchMode: LaunchMode.externalApplication);
+    }
+  }
+
+  String addLocaleToUrl(String locale) {
+    const targetPath = '/lp/jars';
+    final pathIndex = landingUrl.indexOf(targetPath);
+
+    if (pathIndex == -1) {
+      return landingUrl;
+    }
+
+    final prefix = landingUrl.substring(0, pathIndex);
+    final suffix = landingUrl.substring(pathIndex);
+    return '$prefix/$locale$suffix';
   }
 }
